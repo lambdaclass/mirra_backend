c981dd2b23de4ca56ec3fce2f89f71a7bd12a9a8
Amin Arria <arria.amin@gmail.com>
2023-09-11 22:02:52 +0200

    Section for local development helper functions

--- lib/lambda_game_engine.ex
+++ lib/lambda_game_engine.ex
@@ -5,6 +5,14 @@ defmodule LambdaGameEngine do
 
   use Rustler, otp_app: :lambda_game_engine, crate: :lambda_game_engine
 
+  ## Utility functions that are meant ONLY for local development
+  if Mix.env == :dev do
+    def local_config(path \\ "priv/config.json") do
+      {:ok, config} = File.read(path)
+      parse_config(config)
+    end
+  end
+
   # When loading a NIF module, dummy clauses for all NIF function are required.
   # NIF dummies usually just error out when called when the NIF is not loaded, as that should never normally happen.
   @spec parse_config(binary()) :: map()
