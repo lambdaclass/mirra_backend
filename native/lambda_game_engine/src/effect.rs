860393f88e0901eb4a643b4d84bf50749b8a726e
Amin Arria <arria.amin@gmail.com>
2023-09-20 22:55:37 +0200

    Adapt config to new docs

--- native/lambda_game_engine/src/effect.rs
+++ native/lambda_game_engine/src/effect.rs
@@ -4,21 +4,27 @@ use serde::Deserialize;
 #[derive(Deserialize, NifMap, Clone)]
 pub struct Effect {
     pub name: String,
-    duration_ms: u64,
-    trigger_interval_ms: u64,
-    mechanic: Mechanic,
+    effect_time_type: TimeType,
+    player_attributes: Vec<AttributeChange>,
+    projectile_attributes: Vec<AttributeChange>
+}
+
+#[derive(Deserialize, NifMap, Clone)]
+pub struct AttributeChange {
+    attribute: String, // TODO: Can this be force to be certain things? Maybe an enum
+    modifier: AttributeModifier,
+    value: String, // TODO: Figure out how to do dynamic types here
 }
 
 #[derive(Deserialize, NifTaggedEnum, Clone)]
-pub enum Mechanic {
-    HealthChange(i64),
-    MaxHealthPercentageChange(i64),
-    DamagePercentageChange(i64),
-    DefensePercentageChange(i64),
-    SpeedPercentageChange(i64),
-    SizePercentageChange(i64),
-    CooldownMaxPercentageChange(i64),
-    ProjectileSpeedPercentageChange(i64),
-    Piercing,
-    Disarm,
+pub enum AttributeModifier {
+    Additive, Multiplicative, Override
+}
+
+#[derive(Deserialize, NifTaggedEnum, Clone)]
+pub enum TimeType {
+    Instant,
+    Permanent,
+    Duration{duration_ms: u64},
+    Periodic{instant_applicaiton: bool, interval_ms: u64, trigger_count: u64},
 }
