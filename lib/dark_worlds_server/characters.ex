dad461095282e424c23071342bddfba216c6e683
lotuuu <nicolas@lotufo.com.ar>
2023-12-13 12:29:59 -0300

    Create Characters module

---
+++ lib/dark_worlds_server/characters.ex
@@ -0,0 +1,51 @@
+defmodule DarkWorldsServer.Characters do
+  alias DarkWorldsServer.Character
+  alias DarkWorldsServer.CharacterSkill
+  alias DarkWorldsServer.Effect
+  alias DarkWorldsServer.Repo
+  alias DarkWorldsServer.Skill
+
+  def insert_character(attrs \\ %{}) do
+    %Character{}
+    |> Character.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def insert_skill(attrs \\ %{}) do
+    %Skill{}
+    |> Skill.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def insert_effect(attrs \\ %{}) do
+    %Effect{}
+    |> Effect.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def insert_character_skill(attrs \\ %{}) do
+    %CharacterSkill{}
+    |> CharacterSkill.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def get_character(id), do: Repo.get(Character, id)
+
+  def get_skill(id), do: Repo.get(Skill, id)
+
+  def get_effect(id), do: Repo.get(Effect, id)
+
+  def get_character_skill(id), do: Repo.get(CharacterSkill, id)
+
+  def delete_all_characters(), do: Repo.delete_all(Character)
+  def delete_all_skills(), do: Repo.delete_all(Skill)
+  def delete_all_effects(), do: Repo.delete_all(Effect)
+  def delete_all_character_skills(), do: Repo.delete_all(CharacterSkill)
+
+  def delete_all() do
+    delete_all_characters()
+    delete_all_skills()
+    delete_all_effects()
+    delete_all_character_skills()
+  end
+end
