7bc12c2e7c925bd51ef7e90dc9cdcbc06631b1df
lotuuu <nicolas@lotufo.com.ar>
2023-12-15 15:01:11 -0300

    Restructure and expand Characters

--- lib/dark_worlds_server/config/characters.ex
+++ lib/dark_worlds_server/config/characters.ex
@@ -9,62 +9,79 @@ defmodule DarkWorldsServer.Config.Characters do
   alias DarkWorldsServer.Config.Characters.Skill
   alias DarkWorldsServer.Repo
 
+  # Characters
   def insert_character(attrs \\ %{}) do
     %Character{}
     |> Character.changeset(attrs)
     |> Repo.insert()
   end
 
+  def get_character(id), do: Repo.get(Character, id)
+
+  def get_character_by_name(name), do: Repo.one(from(c in Character, where: c.name == ^name))
+
+  def delete_all_characters(), do: Repo.delete_all(Character)
+
+  # Skill
   def insert_skill(attrs \\ %{}) do
     %Skill{}
     |> Skill.changeset(attrs)
     |> Repo.insert()
   end
 
+  def get_skill(id), do: Repo.get(Skill, id)
+
+  def get_skills(), do: Repo.all(Skill)
+
+  def get_skill_by_name(name), do: Repo.one(from(s in Skill, where: s.name == ^name))
+
+  def delete_all_skills(), do: Repo.delete_all(Skill)
+
+  # Effect
   def insert_effect(attrs \\ %{}) do
     %Effect{}
     |> Effect.changeset(attrs)
     |> Repo.insert()
   end
 
+  def get_effect(id), do: Repo.get(Effect, id)
+
+  def get_effects(), do: Repo.all(Effect)
+
+  def get_effect_by_name(name), do: Repo.one(from(e in Effect, where: e.name == ^name))
+
+  def delete_all_effects(), do: Repo.delete_all(Effect)
+
+  # CharacterSkill
   def insert_character_skill(attrs \\ %{}) do
     %CharacterSkill{}
     |> CharacterSkill.changeset(attrs)
     |> Repo.insert()
   end
 
+  def delete_all_character_skills(), do: Repo.delete_all(CharacterSkill)
+
+  # Projectile
   def insert_projectile(attrs \\ %{}) do
     %Projectile{}
     |> Projectile.changeset(attrs)
     |> Repo.insert()
   end
 
+  def get_projectile(id), do: Repo.get(Projectile, id)
+
+  def get_projectiles(), do: Repo.all(Projectile)
+
+  def get_projectile_by_name(name), do: Repo.one(from(e in Projectile, where: e.name == ^name))
+
+  def delete_all_projectiles(), do: Repo.delete_all(Projectile)
+
+  # ProjectileEffect
   def insert_projectile_effect(attrs \\ %{}) do
     %ProjectileEffect{}
     |> ProjectileEffect.changeset(attrs)
     |> Repo.insert()
   end
 
-  def get_character(id), do: Repo.get(Character, id)
-  def get_character_by_name(name), do: Repo.one(from(c in Character, where: c.name == ^name))
-
-  def get_skill(id), do: Repo.get(Skill, id)
-  def get_skill_by_name(name), do: Repo.one(from(s in Skill, where: s.name == ^name))
-
-  def get_effect(id), do: Repo.get(Effect, id)
-  def get_effect_by_name(name), do: Repo.one(from(e in Effect, where: e.name == ^name))
-
-  def get_character_skill(id), do: Repo.get(CharacterSkill, id)
-
-  def get_projectile(id), do: Repo.get(Projectile, id)
-  def get_projectile_by_name(name), do: Repo.one(from(e in Projectile, where: e.name == ^name))
-
-  def get_projectile_effect(id), do: Repo.get(ProjectileEffect, id)
-
-  def delete_all_characters(), do: Repo.delete_all(Character)
-  def delete_all_skills(), do: Repo.delete_all(Skill)
-  def delete_all_effects(), do: Repo.delete_all(Effect)
-  def delete_all_character_skills(), do: Repo.delete_all(CharacterSkill)
-  def delete_all_projectiles(), do: Repo.delete_all(Projectile)
   def delete_all_projectile_effects(), do: Repo.delete_all(ProjectileEffect)
 end
