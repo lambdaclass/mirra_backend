e8b58fc24db54eb4662e7596f43f5c96331b1b22
Agustin Escobar <106101218+agustinesco@users.noreply.github.com>
2023-12-19 12:56:44 -0300

    Implement action tracker instead of action list (#113)
    * checout action tracker from skill leap branch
    
    * Run formatter
    
    * Change clear action and can do action
    
    * Run formatter

--- lib/dark_worlds_server/runner_supervisor/runner.ex
+++ lib/dark_worlds_server/runner_supervisor/runner.ex
@@ -341,10 +341,9 @@ defmodule DarkWorldsServer.RunnerSupervisor.Runner do
       kill_count: player.kill_count,
       effects: transform_effects_to_game_effects(player.effects),
       death_count: 0,
-      action: transform_action_to_game_action(player.actions),
+      action: transform_action_to_game_action(player.action),
       direction: transform_angle_to_game_relative_position(player.direction),
-      aoe_position: %GameBackend.Position{x: 0, y: 0},
-      action_duration_ms: player.action_duration_ms
+      aoe_position: %GameBackend.Position{x: 0, y: 0}
     }
     |> transform_player_cooldowns_to_game_player_cooldowns(player)
   end
@@ -428,16 +427,21 @@ defmodule DarkWorldsServer.RunnerSupervisor.Runner do
 
   defp transform_action_to_game_action([]), do: []
   defp transform_action_to_game_action([:nothing | tail]), do: transform_action_to_game_action(tail)
-  defp transform_action_to_game_action([:moving | tail]), do: [:moving | transform_action_to_game_action(tail)]
 
-  defp transform_action_to_game_action([{:using_skill, "1"} | tail]),
-    do: [:attacking | transform_action_to_game_action(tail)]
+  defp transform_action_to_game_action([%{action: :moving, duration: duration} | tail]),
+    do: [%{action: :moving, duration: duration} | transform_action_to_game_action(tail)]
 
-  defp transform_action_to_game_action([{:using_skill, "2"} | tail]),
-    do: [:executingskill2 | transform_action_to_game_action(tail)]
+  defp transform_action_to_game_action([%{action: {:using_skill, "1"}, duration: duration} | tail]),
+    do: [%{action: :attacking, duration: duration} | transform_action_to_game_action(tail)]
 
-  defp transform_action_to_game_action([{:using_skill, "4"} | tail]),
-    do: [:executingskill4 | transform_action_to_game_action(tail)]
+  defp transform_action_to_game_action([%{action: {:using_skill, "2"}, duration: duration} | tail]),
+    do: [%{action: :startingskill1, duration: duration} | transform_action_to_game_action(tail)]
+
+  defp transform_action_to_game_action([%{action: {:using_skill, "3"}, duration: duration} | tail]),
+    do: [%{action: :executingskill1, duration: duration} | transform_action_to_game_action(tail)]
+
+  defp transform_action_to_game_action([%{action: {:using_skill, "4"}, duration: duration} | tail]),
+    do: [%{action: :executingskill4, duration: duration} | transform_action_to_game_action(tail)]
 
   defp transform_killfeed_to_game_killfeed([]), do: []
 
