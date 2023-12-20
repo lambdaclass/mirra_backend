206e7a31fd42f0424a6f592473864b5333f73477
NContinanza <nicolas.continanza@lambdaclass.com>
2023-12-20 19:00:48 -0300

    Fix rust reference

--- native/game_backend/src/player.rs
+++ native/game_backend/src/player.rs
@@ -363,7 +363,7 @@ impl Player {
     pub fn can_perform_attack(&self, skill_key: &String) -> bool {
         // Check if player is still performing an action or if skill is still on cooldown.
         self.can_do_action()
-            && (can_perform_basic_attack(&self) || can_perform_skill_attack(&self, skill_key))
+            && (can_perform_basic_attack(self) || can_perform_skill_attack(self, skill_key))
     }
 }
 
