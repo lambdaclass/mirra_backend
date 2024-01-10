32ab390780c52404971b0994c8974698f9a88229
Nicolas Sanchez <sanchez.nicolas96@gmail.com>
2024-01-10 11:30:17 -0300

    Update config files for backend

--- config/test.exs
+++ config/test.exs
@@ -13,21 +13,5 @@ config :game_backend, GameBackend.Repo,
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
 
-# # We don't run a server during test. If one is required,
-# # you can enable the server option below.
-# config :game_backend, WebWeb.Endpoint,
-#   http: [ip: {127, 0, 0, 1}, port: 4002],
-#   secret_key_base: "QK4nHna6CWP5+KH2khYXzdIAM2GmQ1B7xwDP6fdjhQro1659xfFvC+69Joj/dKyw",
-#   server: false
-
-# In test we don't send emails.
-# config :game_backend, GameBackend.Mailer, adapter: Swoosh.Adapters.Test
-
-# Disable swoosh api client as it is only required for production adapters.
-# config :swoosh, :api_client, false
-
 # Print only warnings and errors during test
 config :logger, level: :warning
-
-# # Initialize plugs at runtime for faster test compilation
-# config :phoenix, :plug_init_mode, :runtime
