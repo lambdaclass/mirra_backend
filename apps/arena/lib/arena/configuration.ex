82621161a4585e06e2770519a8e9dc2ce258d2b4
Amin Arria <arria.amin@gmail.com>
2024-01-18 21:12:15 +0100

    Parse configuration for skills

--- apps/arena/lib/arena/configuration.ex
+++ apps/arena/lib/arena/configuration.ex
@@ -8,6 +8,32 @@ defmodule Arena.Configuration do
       Application.app_dir(:arena, "priv/config.json")
       |> File.read()
 
-    Jason.decode!(config_json, [{:keys, :atoms}])
+    config = Jason.decode!(config_json, [{:keys, :atoms}])
+    skills = parse_skills_config(config.skills)
+    %{config | skills: skills}
+  end
+
+  defp parse_skills_config(skills_config) do
+    Enum.reduce(skills_config, [], fn (skill_config, skills) ->
+      mechanics = parse_mechanics_config(skill_config.mechanics)
+      skill = %{skill_config | mechanics: mechanics}
+      [skill | skills]
+    end)
+  end
+
+  defp parse_mechanics_config(mechanics_config) do
+    Enum.reduce(mechanics_config, [], fn mechanic_config, acc ->
+      mechanic = parse_mechanic_config(mechanic_config)
+      [mechanic | acc]
+    end)
+  end
+
+  defp parse_mechanic_config(mechanic) when map_size(mechanic) > 1 do
+    raise "Config has mechanic with 2 values: #{inspect mechanic}"
+  end
+
+  defp parse_mechanic_config(mechanic) do
+    Map.to_list(mechanic)
+    |> hd()
   end
 end
