56d2c56a93946a4c0151fd0722eeb400b910d384
NContinanza <nicolas.continanza@lambdaclass.com>
2024-01-26 11:43:02 -0300

    Temporarily remove game_client db configuration to avoid errors when starting the server

--- apps/game_client/config/dev.exs
+++ apps/game_client/config/dev.exs
@@ -1,16 +1,5 @@
 import Config
 
-# Configure your database
-config :game_client, GameClient.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
-  database: "game_client_dev",
-  port: 5433,
-  stacktrace: true,
-  show_sensitive_data_on_connection_error: true,
-  pool_size: 10
-
 # For development, we disable any cache and enable
 # debugging and code reloading.
 #
