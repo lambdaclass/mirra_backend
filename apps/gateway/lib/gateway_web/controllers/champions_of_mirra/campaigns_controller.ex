d8a4495a3d7d9480d372b6b891ff456543473927
lotuuu <nicolas@lotufo.com.ar>
2024-01-29 11:40:01 -0300

    Fix GatewayWeb.Utils.format_response/2 calls

--- apps/gateway/lib/gateway_web/controllers/champions_of_mirra/campaigns_controller.ex
+++ apps/gateway/lib/gateway_web/controllers/champions_of_mirra/campaigns_controller.ex
@@ -8,19 +8,19 @@ defmodule GatewayWeb.ChampionsOfMirra.CampaignsController do
   use GatewayWeb, :controller
 
   def get_campaigns(conn, _params) do
-    ChampionsOfMirra.Campaigns.get_campaigns() |> GatewayWeb.Utils.format_response()
+    ChampionsOfMirra.Campaigns.get_campaigns() |> GatewayWeb.Utils.format_response(conn)
   end
 
   def get_campaign(conn, %{"campaign_number" => campaign_number}) do
-    ChampionsOfMirra.Campaigns.get_campaign(campaign_number) |> GatewayWeb.Utils.format_response()
+    ChampionsOfMirra.Campaigns.get_campaign(campaign_number) |> GatewayWeb.Utils.format_response(conn)
   end
 
   def get_level(conn, %{"level_id" => level_id}) do
-    ChampionsOfMirra.Campaigns.get_level(level_id) |> GatewayWeb.Utils.format_response()
+    ChampionsOfMirra.Campaigns.get_level(level_id) |> GatewayWeb.Utils.format_response(conn)
   end
 
   def fight_level(conn, %{"user_id" => user_id, "level_id" => level_id}) do
     ChampionsOfMirra.Campaigns.fight_level(user_id, level_id)
-    |> GatewayWeb.Utils.format_response()
+    |> GatewayWeb.Utils.format_response(conn)
   end
 end
--- apps/gateway/lib/gateway_web/controllers/champions_of_mirra/items_controller.ex
+++ apps/gateway/lib/gateway_web/controllers/champions_of_mirra/items_controller.ex
@@ -9,18 +9,18 @@ defmodule GatewayWeb.ChampionsOfMirra.ItemsController do
 
   def equip_item(conn, %{"user_id" => user_id, "item_id" => item_id, "unit_id" => unit_id}) do
     ChampionsOfMirra.Units.equip_item(user_id, item_id, unit_id)
-    |> GatewayWeb.Utils.format_response()
+    |> GatewayWeb.Utils.format_response(conn)
   end
 
   def unequip_item(conn, %{"user_id" => user_id, "item_id" => item_id}) do
-    ChampionsOfMirra.Units.unequip_item(user_id, item_id) |> GatewayWeb.Utils.format_response()
+    ChampionsOfMirra.Units.unequip_item(user_id, item_id) |> GatewayWeb.Utils.format_response(conn)
   end
 
   def get_item(conn, %{"item_id" => item_id}) do
-    ChampionsOfMirra.Items.get_item(item_id) |> GatewayWeb.Utils.format_response()
+    ChampionsOfMirra.Items.get_item(item_id) |> GatewayWeb.Utils.format_response(conn)
   end
 
   def level_up(conn, %{"user_id" => user_id, "item_id" => item_id}) do
-    ChampionsOfMirra.Items.level_up(user_id, item_id) |> GatewayWeb.Utils.format_response()
+    ChampionsOfMirra.Items.level_up(user_id, item_id) |> GatewayWeb.Utils.format_response(conn)
   end
 end
