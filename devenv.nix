f7a2b24e6fb664e0fa43a9c032f7ab1919601550
samoht9277 <totobighouse@gmail.com>
2024-02-01 17:07:58 -0300

    Added devenv.

---
+++ devenv.nix
@@ -0,0 +1,56 @@
+{ pkgs, ... }:
+
+{
+  enterShell = ''
+    make start
+  ''; 
+
+  languages = {
+    elixir = {
+      enable = true;
+      package = pkgs.beam.packages.erlangR26.elixir_1_15;
+    };
+
+    erlang = {
+      enable = true;
+    };
+
+    rust = {
+      enable = true;
+    };
+
+    javascript = {
+        enable = true;
+    };
+  };
+
+  packages = [
+    pkgs.inotify-tools
+    pkgs.gnumake
+  ];
+
+  services = {
+    #postgres = {
+    #  enable = true;
+    #  package = pkgs.postgresql_16;
+    #  initialDatabases = [
+    #    { name = "arena_db"; }
+    #    { name = "game_client_db"; }
+    #  ];
+    #  port = 5432;
+    #  listen_addresses = "127.0.0.1";
+    #  initialScript = "CREATE USER postgres WITH PASSWORD 'postgres';";
+    #};
+    #caddy = {
+    #  enable = true;
+    #
+    #  # Replace with actual domain.
+    #  config = "
+    #    localhost {
+    #      reverse_proxy localhost:4000
+    #    }
+    #  ";
+    #};
+  };
+}
+
