47061ef0de5d659153c0cca2c3e0fadf27967d8f
samoht9277 <totobighouse@gmail.com>
2024-02-02 11:43:11 -0300

    Fixed error on MacOS and specified OS-dependant packages.

--- devenv.nix
+++ devenv.nix
@@ -1,56 +1,28 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }: {
 
-{
-  processes = {
-    main.exec = "make start";
-  };
+  packages = [
+    pkgs.gnumake
+  ] ++ lib.optionals pkgs.stdenv.isLinux [ # Packages only for Linux
+    pkgs.inotify-tools
+  ] ++ lib.optionals pkgs.stdenv.isDarwin [ #Packages only for MacOS
+    #pkgs.darwin.apple_sdk.frameworks.Security
+  ];
 
   languages = {
     elixir = {
       enable = true;
       package = pkgs.beam.packages.erlangR26.elixir_1_15;
     };
-
     erlang = {
       enable = true;
     };
-
-    rust = {
-      enable = true;
-    };
-
+   
     javascript = {
       enable = true;
     };
   };
 
-  packages = [
-    pkgs.inotify-tools
-    pkgs.gnumake
-  ];
-
-  services = {
-    #postgres = {
-    #  enable = true;
-    #  package = pkgs.postgresql_16;
-    #  initialDatabases = [
-    #    { name = "arena_db"; }
-    #    { name = "game_client_db"; }
-    #  ];
-    #  port = 5432;
-    #  listen_addresses = "127.0.0.1";
-    #  initialScript = "CREATE USER postgres WITH PASSWORD 'postgres';";
-    #};
-    #caddy = {
-    #  enable = true;
-    #
-    #  # Replace with actual domain.
-    #  config = "
-    #    localhost {
-    #      reverse_proxy localhost:4000
-    #    }
-    #  ";
-    #};
+  processes = {
+    main.exec = "make start";
   };
 }
-
