fb814504b489ad2fc08e4620d864b1d691fc0516
samoht9277 <totobighouse@gmail.com>
2024-02-02 12:38:51 -0300

    Added MacOS and Linux compatibility on the same decive.

--- devenv.nix
+++ devenv.nix
@@ -1,11 +1,20 @@
 { pkgs, lib, ... }: {
 
+  # Example for passing env var to make and caddy to deploy the app automatically
+  # env.DOMAIN = "brazil-testing.curseofmirra.com";
+
+  processes = {
+    main.exec = "make start";
+  };
+
   packages = [
     pkgs.gnumake
+    #pkgs.rustc
+    #pkgs.cargo
   ] ++ lib.optionals pkgs.stdenv.isLinux [ # Packages only for Linux
     pkgs.inotify-tools
   ] ++ lib.optionals pkgs.stdenv.isDarwin [ #Packages only for MacOS
-    #pkgs.darwin.apple_sdk.frameworks.Security
+
   ];
 
   languages = {
@@ -13,16 +22,18 @@
       enable = true;
       package = pkgs.beam.packages.erlangR26.elixir_1_15;
     };
+
     erlang = {
       enable = true;
+    };
+
+    rust = {
+      enable = true;
+      components = [ "rustc" "cargo" "clippy" "rustfmt" "rust-analyzer" ];
     };
    
     javascript = {
       enable = true;
     };
-  };
-
-  processes = {
-    main.exec = "make start";
   };
 }
