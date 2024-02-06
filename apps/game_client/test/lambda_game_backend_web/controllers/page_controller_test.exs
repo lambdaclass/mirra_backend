c98da935a2e891c92cb818341cc6cdb336c8b751
lotuuu <nicolas@lotufo.com.ar>
2024-02-06 12:21:15 -0300

    Remove unused tests

--- apps/game_client/test/lambda_game_backend_web/controllers/page_controller_test.exs
+++ apps/game_client/test/lambda_game_backend_web/controllers/page_controller_test.exs
@@ -1,8 +1,8 @@
 defmodule GameClientWeb.PageControllerTest do
   use GameClientWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Lambda Game Backend"
-  end
+  # test "GET /", %{conn: conn} do
+  #   conn = get(conn, ~p"/")
+  #   assert html_response(conn, 200) =~ "Lambda Game Backend"
+  # end
 end
