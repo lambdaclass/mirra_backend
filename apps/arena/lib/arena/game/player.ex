c06a94c3b0f1cad1bcc1ca46a5086ce43d3ccce6
Manuel Camejo <mcamejo@fi.uba.ar>
2024-02-21 21:13:08 -0300

    restore config values

--- apps/arena/lib/arena/game/player.ex
+++ apps/arena/lib/arena/game/player.ex
@@ -166,7 +166,7 @@ defmodule Arena.Game.Player do
 
         player =
           add_action(player, action_name, skill.execution_duration_ms)
-          |> change_stamina(0)
+          |> change_stamina(-1)
           |> put_in([:direction], skill_direction)
           |> put_in([:is_moving], false)
 
