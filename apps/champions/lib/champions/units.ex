253f42cc9985d795c912c5c3dafb361abfd167d4
lotuuu <nicolas@lotufo.com.ar>
2024-02-23 16:10:25 -0300

    General fixes to Champions.Units

--- apps/champions/lib/champions/units.ex
+++ apps/champions/lib/champions/units.ex
@@ -8,18 +8,16 @@ defmodule Champions.Units do
   @doc """
   Marks a unit as selected for a user. Units cannot be selected to the same slot.
 
-  Returns :not_found if unit doesn't exist or if it's now owned by the user.
+  Returns `:not_found` if unit doesn't exist or if it's now owned by the user.
   Returns the unit's new state if succesful.
   """
   def select_unit(_user_id, _unit_id, nil), do: {:error, :no_slot}
-  def select_unit(_user_id, _unit_id, slot) when slot > 4, do: {:error, :out_of_bounds}
+  def select_unit(_user_id, _unit_id, slot) when slot not in 1..5, do: {:error, :out_of_bounds}
 
   def select_unit(user_id, unit_id, slot) do
-    if Units.get_selected_units(user_id) |> Enum.any?(&(&1.slot == slot)) do
-      {:error, :slot_occupied}
-    else
-      Units.select_unit(user_id, unit_id, slot)
-    end
+    if Units.get_selected_units(user_id) |> Enum.any?(&(&1.slot == slot)),
+      do: {:error, :slot_occupied},
+      else: Units.select_unit(user_id, unit_id, slot)
   end
 
   def unselect_unit(user_id, unit_id) do
