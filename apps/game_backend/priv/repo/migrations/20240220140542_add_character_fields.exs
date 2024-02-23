4dc222a5379a7dea382edec494a7422a3c311aa6
lotuuu <nicolas@lotufo.com.ar>
2024-02-23 17:19:58 -0300

    Fix merge

--- apps/game_backend/priv/repo/migrations/20240220140542_add_character_fields.exs
+++ apps/game_backend/priv/repo/migrations/20240220140542_add_character_fields.exs
@@ -6,10 +6,6 @@ defmodule GameBackend.Repo.Migrations.AddCharacterFields do
       add :title, :string
       add :lore, :text
       add :class, :string
-      add :base_health, :integer
-      add :base_attack, :integer
-      add :base_speed, :integer
-      add :base_defense, :integer
     end
   end
 end
