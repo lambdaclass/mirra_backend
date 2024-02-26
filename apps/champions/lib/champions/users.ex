6e0d89e71abb6a93f9e15ad3883648fd4a15fdeb
lotuuu <nicolas@lotufo.com.ar>
2024-02-26 11:12:09 -0300

    Return ok tuple in register user

--- apps/champions/lib/champions/users.ex
+++ apps/champions/lib/champions/users.ex
@@ -23,7 +23,7 @@ defmodule Champions.Users do
         add_sample_items(user)
         add_sample_currencies(user)
 
-        Users.get_user(user.id)
+        {:ok, Users.get_user(user.id)}
 
       {:error, changeset} ->
         [[first_error | _other_errors] | _other_fields_errors] =
