7eb30ce5a79f5cfe0082873173f5ff69d2776bab
lotuuu <nicolas@lotufo.com.ar>
2024-03-12 13:18:23 -0300

    Abstract lowest-highest attribute strategies

--- apps/game_backend/lib/game_backend/units/skills/targeting_strategy.ex
+++ apps/game_backend/lib/game_backend/units/skills/targeting_strategy.ex
@@ -10,27 +10,21 @@ defmodule GameBackend.Units.Skills.TargetingStrategy do
   def cast("random"), do: {:ok, "random"}
   def cast("nearest"), do: {:ok, "nearest"}
   def cast("furthest"), do: {:ok, "furthest"}
-  def cast("min_health"), do: {:ok, "min_health"}
-  def cast("max_health"), do: {:ok, "max_health"}
-  def cast("min_shield"), do: {:ok, "min_shield"}
-  def cast("max_shield"), do: {:ok, "max_shield"}
   def cast("frontline"), do: {:ok, "frontline"}
   def cast("backline"), do: {:ok, "backline"}
   def cast(%{"factions" => factions}), do: {:ok, {"factions", factions}}
   def cast(%{"classes" => classes}), do: {:ok, {"classes", classes}}
+  def cast({"lowest", attribute}), do: {:ok, {"lowest", attribute}}
+  def cast({"highest", attribute}), do: {:ok, {"highest", attribute}}
   def cast(_), do: :error
 
   def load(string), do: {:ok, targeting_strategy_from_string(string)}
 
-  def dump(time), do: {:ok, targeting_strategy_to_string(time)}
+  def dump(strategy), do: {:ok, targeting_strategy_to_string(strategy)}
 
   defp targeting_strategy_to_string("random"), do: "random"
   defp targeting_strategy_to_string("nearest"), do: "nearest"
   defp targeting_strategy_to_string("furthest"), do: "furthest"
-  defp targeting_strategy_to_string("min_health"), do: "min_health"
-  defp targeting_strategy_to_string("max_health"), do: "max_health"
-  defp targeting_strategy_to_string("min_shield"), do: "min_shield"
-  defp targeting_strategy_to_string("max_shield"), do: "max_shield"
   defp targeting_strategy_to_string("frontline"), do: "frontline"
   defp targeting_strategy_to_string("backline"), do: "backline"
 
@@ -40,15 +34,17 @@ defmodule GameBackend.Units.Skills.TargetingStrategy do
   defp targeting_strategy_to_string({"classes", classes}),
     do: "classes,#{Enum.join(classes, ",")}"
 
+  defp targeting_strategy_to_string({"lowest", attribute}),
+    do: "lowest,#{attribute}"
+
+  defp targeting_strategy_to_string({"highest", attribute}),
+    do: "highest,#{attribute}"
+
   defp targeting_strategy_to_string(_), do: nil
 
   defp targeting_strategy_from_string("random"), do: "random"
   defp targeting_strategy_from_string("nearest"), do: "nearest"
   defp targeting_strategy_from_string("furthest"), do: "furthest"
-  defp targeting_strategy_from_string("min_health"), do: "min_health"
-  defp targeting_strategy_from_string("max_health"), do: "max_health"
-  defp targeting_strategy_from_string("min_shield"), do: "min_shield"
-  defp targeting_strategy_from_string("max_shield"), do: "max_shield"
   defp targeting_strategy_from_string("frontline"), do: "frontline"
   defp targeting_strategy_from_string("backline"), do: "backline"
 
@@ -59,6 +55,12 @@ defmodule GameBackend.Units.Skills.TargetingStrategy do
 
       ["classes", classes] ->
         {"classes", classes}
+
+      ["lowest", attribute] ->
+        {"lowest", attribute}
+
+      ["highest", attribute] ->
+        {"highest", attribute}
 
       _ ->
         nil
