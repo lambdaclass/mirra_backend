07334d2af06f0b78a36395f5b4d6f100e4fe59f3
agustinesco <agustin.escobar@lambdaclass.com>
2024-03-12 19:07:10 -0300

    Add bot manager to umbrella apps

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MirraBackend.MixProject do
   def project do
     [
       apps_path: "apps",
-      apps: [:arena, :champions, :game_client, :gateway, :game_backend],
+      apps: [:arena, :champions, :game_client, :gateway, :game_backend, :bot_manager],
       version: "0.1.0",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
