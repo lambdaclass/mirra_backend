ca83915c69dd215b0e3fb727312d2634fd55bbb9
lotuuu <nicolas@lotufo.com.ar>
2024-03-13 18:19:53 -0300

    Merge branch 'main' into add-seeds

--- apps/arena/lib/arena/game_socket_handler.ex
+++ apps/arena/lib/arena/game_socket_handler.ex
@@ -25,8 +25,7 @@ defmodule Arena.GameSocketHandler do
     Logger.info("Websocket INIT called")
     Phoenix.PubSub.subscribe(Arena.PubSub, state.game_id)
 
-    {:ok, %{player_id: player_id, game_config: config}} =
-      GameUpdater.join(state.game_pid, state.client_id)
+    {:ok, %{player_id: player_id, game_config: config}} = GameUpdater.join(state.game_pid, state.client_id)
 
     state =
       Map.put(state, :player_id, player_id)
@@ -157,7 +156,13 @@ defmodule Arena.GameSocketHandler do
     ## TODO: This will break once a skill has more than 1 mechanic, until then
     ##   we can use this "shortcut" and deal with it when the time comes
     [{_mechanic, params}] = skill.mechanics
-    extra_params = %{targetting_radius: params[:range], targetting_angle: params[:angle]}
+
+    extra_params = %{
+      targetting_radius: params[:radius],
+      targetting_angle: params[:angle],
+      targetting_range: params[:range]
+    }
+
     {key, Map.merge(skill, extra_params)}
   end
 end
