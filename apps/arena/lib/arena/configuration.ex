7385675fb1fdcbadd5d5a62705f4d2dd187df5a1
Amin Arria <arria.amin@gmail.com>
2024-03-14 19:12:20 +0100

    Add cooldown_mechanism to configuration

--- apps/arena/lib/arena/configuration.ex
+++ apps/arena/lib/arena/configuration.ex
@@ -21,10 +21,23 @@ defmodule Arena.Configuration do
 
   defp parse_skills_config(skills_config) do
     Enum.reduce(skills_config, [], fn skill_config, skills ->
-      mechanics = parse_mechanics_config(skill_config.mechanics)
-      skill = %{skill_config | mechanics: mechanics}
+      skill = parse_skill_config(skill_config)
       [skill | skills]
     end)
+  end
+
+  defp parse_skill_config(%{cooldown_mechanism: "stamina", stamina_cost: cost} = skill_config) when cost >= 0 do
+    mechanics = parse_mechanics_config(skill_config.mechanics)
+    %{skill_config | mechanics: mechanics}
+  end
+
+  defp parse_skill_config(%{cooldown_mechanism: "time", cooldown_ms: cooldown} = skill_config) when cooldown >= 0 do
+    mechanics = parse_mechanics_config(skill_config.mechanics)
+    %{skill_config | mechanics: mechanics}
+  end
+
+  defp parse_skill_config(skill_config) do
+    raise "Invalid Skill config for `#{skill_config[:name]}`: cooldown_mechanism or corresponding attribute is invalid"
   end
 
   defp parse_mechanics_config(mechanics_config) do
