d686e2943b879292611a2e4bf6bbd945d0974c57
lotuuu <nicolas@lotufo.com.ar>
2024-03-15 17:06:39 -0300

    Add game_backend deploy scripts

---
+++ devops/deploy.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+. "$HOME/.cargo/env"
+set -ex
+
+if [ -d "/tmp/mirra_backend" ]; then
+	rm -rf /tmp/mirra_backend
+fi
+
+cd /tmp
+git clone https://github.com/lambdaclass/mirra_backend.git --branch ${BRANCH_NAME}
+
+chmod +x devops/entrypoint.sh
+
+mix local.hex --force && mix local.rebar --force
+mix deps.get --only $MIX_ENV
+mix deps.compile
+mix compile
+mix phx.gen.release
+mix release --overwrite
+
+rm -rf $HOME/mirra_backend
+mv /tmp/mirra_backend $HOME/
+
+mkdir -p $HOME/.config/systemd/user/
+cat <<EOF >$HOME/.config/systemd/user/game_backend.service
+[Unit]
+Description=GameBackend
+Requires=network-online.target
+After=network-online.target
+
+[Service]
+WorkingDirectory=$HOME/mirra_backend
+Restart=on-failure
+ExecStart=$HOME/mirra_backend/devops/entrypoint.sh
+ExecReload=/bin/kill -HUP
+KillSignal=SIGTERM
+EnvironmentFile=$HOME/.env
+LimitNOFILE=4000
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl --user enable game_backend
+
+cat <<EOF >$HOME/.env
+PHX_HOST=${PHX_HOST}
+PHX_SERVER=${PHX_SERVER}
+SECRET_KEY_BASE=${SECRET_KEY_BASE}
+PORT=${PORT}
+EOF
+
+systemctl --user stop game_backend
+
+systemctl --user daemon-reload
+systemctl --user start game_backend
---
+++ devops/entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+set -ex
+
+$HOME/mirra_backend/_build/prod/rel/game_backend/bin/game_backend start
