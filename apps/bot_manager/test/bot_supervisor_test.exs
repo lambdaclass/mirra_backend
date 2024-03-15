371c884b5c7f5b7d3ffe8f22be9a56c4b284d01e
agustinesco <agustin.escobar@lambdaclass.com>
2024-03-15 13:19:25 -0300

    Remove unnecessary test

--- apps/bot_manager/test/bot_supervisor_test.exs
+++ apps/bot_manager/test/bot_supervisor_test.exs
@@ -1,8 +1,4 @@
 defmodule BotSupervisorTest do
   use ExUnit.Case
-  doctest BotSupervisor
-
-  test "greets the world" do
-    assert BotSupervisor.hello() == :world
-  end
+  doctest BotManager.BotSupervisor
 end
