6b87fb38f417fc5a171ccc31ff4b2d5d75589a3b
samoht9277 <totobighouse@gmail.com>
2024-03-20 18:18:54 -0300

    Added user reference in script.

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -15,23 +15,23 @@ mix deps.compile
 mix compile
 mix release --overwrite
 
-rm -rf /home/mirra_backend
-mv /tmp/mirra_backend /home/
+rm -rf /home/dev/mirra_backend
+mv /tmp/mirra_backend /home/dev/
 
-mkdir -p /home/.config/systemd/user/
-cat <<EOF >/home/.config/systemd/user/game_backend.service
+mkdir -p /home/dev/.config/systemd/user/
+cat <<EOF >/home/dev/.config/systemd/user/game_backend.service
 [Unit]
 Description=GameBackend
 Requires=network-online.target
 After=network-online.target
 
 [Service]
-WorkingDirectory=/home/mirra_backend
+WorkingDirectory=/home/dev/mirra_backend
 Restart=on-failure
-ExecStart=/home/mirra_backend/devops/entrypoint.sh
+ExecStart=/home/dev/mirra_backend/devops/entrypoint.sh
 ExecReload=/bin/kill -HUP
 KillSignal=SIGTERM
-EnvironmentFile=/home/mirra_backend/.env
+EnvironmentFile=/home/dev/.env
 LimitNOFILE=4000
 
 [Install]
