a9cfa8baba07269b42a4b8231005980b95d5ea31
Nicolas Sanchez <sanchez.nicolas96@gmail.com>
2024-03-26 11:22:46 -0300

    Update the deploy workflow and the code to use gh vars

--- apps/arena_load_test/lib/arena_load_test/socket_supervisor.ex
+++ apps/arena_load_test/lib/arena_load_test/socket_supervisor.ex
@@ -36,6 +36,6 @@ defmodule ArenaLoadTest.SocketSupervisor do
     end
   end
 
-  def get_server_url("Brazil"), do: "brazil-testing.curseofmirra.com"
-  def get_server_url("Europe"), do: "europe-testing.curseofmirra.com"
+  def get_server_url("Brazil"), do: System.get_env("BRAZIL_HOST")
+  def get_server_url("Europe"), do: System.get_env("EUROPE_HOST")
 end
