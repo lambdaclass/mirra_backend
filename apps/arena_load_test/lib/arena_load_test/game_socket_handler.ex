8bed98cf3cbef263e9ef45cd4b29f275f7d4ed54
Nicolas Sanchez <sanchez.nicolas96@gmail.com>
2024-04-07 18:59:51 -0300

    Spawn a new client when a player finishes it match. This is to make the players in game constant

--- apps/arena_load_test/lib/arena_load_test/game_socket_handler.ex
+++ apps/arena_load_test/lib/arena_load_test/game_socket_handler.ex
@@ -95,6 +95,7 @@ defmodule ArenaLoadTest.GameSocketHandler do
         raise KeyError, message: "Player with ID #{client_id} doesn't exist."
     end
 
+    SocketSupervisor.add_new_client(client_id)
     Logger.info("Player websocket terminated. Game Ended.")
     exit(:normal)
   end
