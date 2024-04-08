8993d40a84470012294ffe3d2f9b6735c39bdf0a
samoht9277 <totobighouse@gmail.com>
2024-04-08 12:58:28 -0300

    Removed RELEASE workflow input on GitHub Actions.

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -26,13 +26,6 @@ mv /tmp/mirra_backend $HOME/
 
 mkdir -p $HOME/.config/systemd/user/
 
-existing_service=$(ls $HOME/.config/systemd/user/*.service 2>/dev/null)
-
-if [[ $(wc -l <<<$existing_service) > 1 || "$(basename ${existing_service})" != "${RELEASE}.service" ]]; then
-	echo "The release you are trying to deploy is not the same as the installed"
-	exit 1
-fi
-
 cat <<EOF >$HOME/.config/systemd/user/${RELEASE}.service
 [Unit]
 Description=$RELEASE
