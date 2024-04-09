cf85eadb2eabc7b008f59ba9883c4f00b5e2ced6
agustinesco <agustin.escobar@lambdaclass.com>
2024-04-09 13:42:59 -0300

    Add moduledoc to missing files

--- apps/bot_manager/lib/math/vector.ex
+++ apps/bot_manager/lib/math/vector.ex
@@ -1,4 +1,8 @@
 defmodule BotManager.Math.Vector do
+  @moduledoc """
+  Module to handle math operations with vectors
+  """
+
   def sub(vector, value) when is_integer(value) or is_float(value) do
     Map.new(vector, fn {_key, vector_value} ->
       vector_value - value
@@ -11,9 +15,15 @@ defmodule BotManager.Math.Vector do
     end)
   end
 
-  def mult(vector, value) do
+  def mult(vector, value) when is_integer(value) or is_float(value) do
     Map.new(vector, fn {key, vector_value} ->
       {key, vector_value * value}
+    end)
+  end
+
+  def mult(first_vector, second_vector) do
+    Map.merge(first_vector, second_vector, fn _key, first_vector_value, second_vector_value ->
+      first_vector_value * second_vector_value
     end)
   end
 end
