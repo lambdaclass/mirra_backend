d9c6799dae1292c1e9fb1123ac4aaed2f34dafb4
Amin Arria <arria.amin@gmail.com>
2024-04-14 10:56:02 +0200

    Upgrade telemetry_metrics to 1.0 (stable API, no changes)

--- apps/game_client/mix.exs
+++ apps/game_client/mix.exs
@@ -47,7 +47,7 @@ defmodule GameClient.MixProject do
       {:tailwind, "~> 0.2.0", runtime: Mix.env() == :dev},
       {:swoosh, "~> 1.3"},
       {:finch, "~> 0.13"},
-      {:telemetry_metrics, "~> 0.6"},
+      {:telemetry_metrics, "~> 1.0"},
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
