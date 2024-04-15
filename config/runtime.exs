2ab3d76d29c2d4d9f0e75e27c0588ca81fe9c640
agustinesco <agustin.escobar@lambdaclass.com>
2024-04-15 18:41:01 -0300

    Fix client port

--- config/runtime.exs
+++ config/runtime.exs
@@ -162,7 +162,7 @@ if config_env() == :prod do
       # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
       # for details about using IPv6 vs IPv4 and loopback vs public addresses.
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: port
+      port: "3000"
     ],
     secret_key_base: secret_key_base
 
