4da15ea4d79b4107a3973846a56d57aba086c371
agustinesco <agustin.escobar@lambdaclass.com>
2024-04-15 10:39:52 -0300

    Fix credo complain

--- apps/arena/lib/arena/game/crate.ex
+++ apps/arena/lib/arena/game/crate.ex
@@ -1,4 +1,7 @@
 defmodule Arena.Game.Crate do
+  @moduledoc """
+  Module to handle crate logic
+  """
   def take_damage(crate, damage) do
     update_in(crate, [:aditional_info, :health], fn current_health -> max(current_health - damage, 0) end)
   end
