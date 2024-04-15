754335c3effd3df69d7766eeed6bc71325e17712
Nicolas Sanchez <sanchez.nicolas96@gmail.com>
2024-04-15 14:39:33 -0300

    Add :tools app deps to arena and arenaloadtest

--- apps/arena_load_test/mix.exs
+++ apps/arena_load_test/mix.exs
@@ -18,7 +18,7 @@ defmodule ArenaLoadTest.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :observer],
+      extra_applications: [:logger, :observer, :tools],
       mod: {ArenaLoadTest.Application, []}
     ]
   end
