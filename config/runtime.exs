1a687f3f129444ad4b326d3c5e9677f0d2b83686
Nicolas Sanchez <sanchez.nicolas96@gmail.com>
2024-04-15 15:08:54 -0300

    Move game client to port 3000

--- config/runtime.exs
+++ config/runtime.exs
@@ -150,7 +150,7 @@ if config_env() == :prod do
       """
 
   host = System.get_env("PHX_HOST") || "example.com"
-  port = String.to_integer(System.get_env("PORT") || "3000")
+  port = 3000
 
   config :game_client, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
