93ead768e139639353bee4f45e737fe05bbb0730
lotuuu <nicolas@lotufo.com.ar>
2024-04-16 13:49:55 -0300

    Fix sample user and add doc to Characters

--- apps/game_backend/lib/game_backend/units/characters.ex
+++ apps/game_backend/lib/game_backend/units/characters.ex
@@ -12,12 +12,31 @@ defmodule GameBackend.Units.Characters do
   # Characters #
   ##############
 
+  @doc """
+  Inserts a Character.
+
+  ## Examples
+
+      iex> insert_character(%{field: value})
+      {:ok, %Character{}}
+
+      iex> insert_character(%{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+  """
   def insert_character(attrs \\ %{}) do
     %Character{}
     |> Character.changeset(attrs)
     |> Repo.insert()
   end
 
+  @doc """
+  Update a Character.
+
+  ## Examples
+
+      iex> update_character(%Character{name: "Muflus"}, %{name: "H4ck"})
+      {:ok, %Character{name: "H4ck"}}
+  """
   def update_character(character, attrs \\ %{}) do
     character
     |> Character.changeset(attrs)
@@ -42,18 +61,75 @@ defmodule GameBackend.Units.Characters do
     |> Repo.transaction()
   end
 
+  @doc """
+  Get a Character by id.
+
+  ## Examples
+
+      iex> get_character(item_template_id)
+      {:ok, %Character{}}
+
+      iex> get_character(wrong_id)
+      {:error, :not_found}
+  """
   def get_character(id), do: Repo.get(Character, id) |> Repo.preload([:basic_skill, :ultimate_skill])
 
+  @doc """
+  Get all characters.
+
+  ## Examples
+      iex> get_characters()
+      [%Character{}]
+  """
   def get_characters(), do: Repo.all(Character) |> Repo.preload([:basic_skill, :ultimate_skill])
 
+  @doc """
+  Get a Character by id.
+
+  ## Examples
+
+      iex> get_character(character_name)
+      %Character{}
+
+      iex> get_character(wrong_character_name)
+      nil
+  """
   def get_character_by_name(name),
     do: Repo.one(from(c in Character, where: c.name == ^name)) |> Repo.preload([:basic_skill, :ultimate_skill])
 
+  @doc """
+  Delete all characters from the database.
+  """
   def delete_all_characters(), do: Repo.delete_all(Character)
 
-  def get_characters_by_rank(rank),
-    do: Repo.all(from(c in Character, where: ^rank in c.ranks_dropped_in))
+  @doc """
+  Get all characters with given quality.
 
+  ## Examples
+
+      iex> get_characters_by_quality(Champions.Units.get_quality(:epic))
+      [%Character%{quality: 5}]
+  """
+  def get_characters_by_quality(quality), do: Repo.all(from(c in Character, where: ^quality == c.quality))
+
+  @doc """
+  Get all characters with given rank_dropped_in.
+
+  ## Examples
+
+      iex> get_characters_by_rank(Champions.Units.get_rank(:star4))
+      [%Character%{rank: 4}]
+  """
+  def get_characters_by_rank(rank), do: Repo.all(from(c in Character, where: ^rank in c.ranks_dropped_in))
+
+  @doc """
+  Get all characters with given rank_dropped_in.
+
+  ## Examples
+
+  iex> get_characters_by_rank_and_faction(Champions.Units.get_rank(:star4), "Kaline")
+  [%Character%{rank: 4, faction: "Kaline"}]
+  """
   def get_characters_by_rank_and_faction(rank, factions),
     do: Repo.all(from(c in Character, where: ^rank in c.ranks_dropped_in and c.faction in ^factions))
 end
