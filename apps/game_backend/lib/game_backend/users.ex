d187a7082d8307624d337b3b5a49e6dec85877c0
Nicolas Sanchez <sanchez.nicolas96@gmail.com>
2024-04-22 18:45:33 -0300

    Add find and create functions for GoogleUser

--- apps/game_backend/lib/game_backend/users.ex
+++ apps/game_backend/lib/game_backend/users.ex
@@ -13,6 +13,7 @@ defmodule GameBackend.Users do
 
   alias GameBackend.Repo
   alias GameBackend.Users.User
+  alias GameBackend.Users.GoogleUser
 
   @doc """
   Registers a user.
@@ -66,6 +67,18 @@ defmodule GameBackend.Users do
   def get_user_by_username(username) do
     user = Repo.get_by(User, username: username) |> preload()
     if user, do: {:ok, user}, else: {:error, :not_found}
+  end
+
+  def find_or_create_google_user_by_email(email) do
+    case Repo.get_by(GoogleUser, email: email) do
+      nil -> create_google_user_by_email(email)
+      user -> {:ok, user}
+    end
+  end
+
+  defp create_google_user_by_email(email) do
+    GoogleUser.changeset(%GoogleUser{}, %{email: email})
+    |> Repo.insert()
   end
 
   @doc """
