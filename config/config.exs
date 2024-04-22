aea95b0a405813540cf680ef2d257171d34fac95
Nicolas Sanchez <sanchez.nicolas96@gmail.com>
2024-04-22 18:48:41 -0300

    Format elixir code

--- config/config.exs
+++ config/config.exs
@@ -199,7 +199,6 @@ config :ueberauth, Ueberauth.Strategy.Google.OAuth,
   client_id: System.get_env("GOOGLE_CLIENT_ID"),
   client_secret: System.get_env("GOOGLE_CLIENT_SECRET")
 
-
 ############################
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
