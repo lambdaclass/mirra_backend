da295a14abae11db20f1d865daf7aef0f3e76ee3
Nicolas Sanchez <sanchez.nicolas96@gmail.com>
2024-04-23 13:44:49 -0300

    Add quick_game entrypoint in game_client app and also improve its code and view

--- apps/game_client/lib/game_client_web/live/pages/board/game_queue.ex
+++ apps/game_client/lib/game_client_web/live/pages/board/game_queue.ex
@@ -2,8 +2,8 @@ defmodule GameClientWeb.BoardLive.GameQueue do
   require Logger
   use GameClientWeb, :live_view
 
-  def mount(%{"player_id" => player_id, "character" => character, "player_name" => player_name}, _session, socket) do
-    {:ok, assign(socket, player_id: player_id, character: character, player_name: player_name)}
+  def mount(%{"player_id" => player_id, "character" => character, "player_name" => player_name, "game_mode" => game_mode}, _session, socket) do
+    {:ok, assign(socket, player_id: player_id, character: character, player_name: player_name, game_mode: game_mode)}
   end
 
   def handle_event("join_game", %{"game_id" => game_id, "player_id" => player_id}, socket) do
