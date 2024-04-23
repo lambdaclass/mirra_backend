3cda3a0b0ac0fc91af70bcc52e1b217cd764591b
Nicolas Sanchez <sanchez.nicolas96@gmail.com>
2024-04-23 14:45:38 -0300

    Get ueberauth credentials in runtime

--- config/runtime.exs
+++ config/runtime.exs
@@ -221,6 +221,10 @@ end
 # App configuration: gateway #
 ##############################
 
+config :ueberauth, Ueberauth.Strategy.Google.OAuth,
+  client_id: System.get_env("GOOGLE_CLIENT_ID"),
+  client_secret: System.get_env("GOOGLE_CLIENT_SECRET")
+
 ###################################
 # App configuration: configurator #
 ###################################
