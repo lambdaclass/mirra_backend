28b5aa319940d2f1304ff30442a4cc0d76c2a1b7
Nicolas Sanchez <sanchez.nicolas96@gmail.com>
2024-04-26 18:15:30 -0300

    Add validate token endpoint

---
+++ apps/gateway/lib/gateway/controllers/auth_controller.ex
@@ -0,0 +1,21 @@
+defmodule Gateway.Controllers.AuthController do
+  @moduledoc """
+  Controller for users authentication.
+  """
+  use Gateway, :controller
+  alias Gateway.Auth.GoogleTokenManager
+  alias GameBackend.Users
+
+  def validate_token(conn, %{"token_id" => token_id}) do
+    conn = put_resp_content_type(conn, "application/json")
+
+    case GoogleTokenManager.verify_and_validate(token_id) do
+      {:ok, response} ->
+        Users.find_or_create_google_user_by_email(response["email"])
+        send_resp(conn, 200, Jason.encode!(response))
+
+      {:error, error} ->
+        send_resp(conn, 401, Jason.encode!(Map.new(error)))
+    end
+  end
+end
