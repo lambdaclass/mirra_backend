83ed9452fc94e918ad7e2c495eb5c590b04db16c
Amin Arria <arria.amin@gmail.com>
2024-04-26 18:48:24 +0200

    Modify match result endpoint to accept batch instead of single

--- apps/game_backend/lib/game_backend/matches.ex
+++ apps/game_backend/lib/game_backend/matches.ex
@@ -2,12 +2,19 @@ defmodule GameBackend.Matches do
   @moduledoc """
   Matches
   """
+  alias Ecto.Multi
   alias GameBackend.Repo
   alias GameBackend.Matches.ArenaMatchResult
 
-  def create_arena_match_result(attrs) do
-    %ArenaMatchResult{}
-    |> ArenaMatchResult.changeset(attrs)
-    |> Repo.insert()
+  def create_arena_match_results(results) do
+    transaction = Multi.new()
+    {transaction, _} =
+      Enum.reduce(results, {transaction, 1}, fn result, {transaction_acc, idx} ->
+        changeset = ArenaMatchResult.changeset(%ArenaMatchResult{}, result)
+        transaction_acc = Multi.insert(transaction_acc, {:insert, idx}, changeset)
+        {transaction_acc, idx + 1}
+      end)
+
+    Repo.transaction(transaction)
   end
 end
--- apps/gateway/lib/gateway/controllers/arena/match_results_controller.ex
+++ apps/gateway/lib/gateway/controllers/arena/match_results_controller.ex
@@ -6,24 +6,18 @@ defmodule Gateway.Controllers.Arena.MatchResultsController do
   use Gateway, :controller
   alias GameBackend.Matches
 
-  def create(conn, params) do
-    IO.inspect(params)
-    case Matches.create_arena_match_result(params) do
+  def create(conn, %{"results" => results}) do
+    case Matches.create_arena_match_results(results) do
       {:ok, _match_result} ->
         send_resp(conn, 201, "")
 
-      {:error, %Ecto.Changeset{} = changeset} ->
+      {:error, _insert_op, %Ecto.Changeset{} = changeset, _} ->
+        ## TODO: properly handle errors with a better error message
         errors = Ecto.Changeset.traverse_errors(changeset, fn {msg, _opts} -> msg end)
 
         conn
         |> put_status(422)
-        |> json(%{
-          error: %{
-            status: 422,
-            message: "Unprocessable Entity",
-            errors: errors
-          }
-        })
+        |> json(%{status: 422, message: "Unprocessable Entity", errors: errors})
     end
   end
 
