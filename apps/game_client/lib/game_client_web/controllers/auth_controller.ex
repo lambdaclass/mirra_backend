4ef4b71c32d9044fdb779d40fa6b5ea5a9a86fe7
Amin Arria <arria.amin@gmail.com>
2024-05-02 23:25:25 +0200

    Add ueberauth_google to game_client application and use Gateway to verify user

---
+++ apps/game_client/lib/game_client_web/controllers/auth_controller.ex
@@ -0,0 +1,29 @@
+defmodule GameClientWeb.AuthController do
+  use GameClientWeb, :controller
+  plug Ueberauth
+
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, _params) do
+    token = auth.extra.raw_info.token.other_params["id_token"]
+    gateway_url = System.get_env("GATEWAY_URL") || "http://localhost:4001"
+    response =
+      Finch.build(:get, "#{gateway_url}/auth/google/token/#{token}", [{"content-type", "application/json"}])
+      |> Finch.request(GameClient.Finch)
+
+    case response do
+      {:ok, %{status: 200, body: body}} ->
+        body = Jason.decode!(body)
+        conn
+        |> put_flash(:info, "Logged in successfully.")
+        |> put_session(:user_id, get_in(body, ["user", "id"]))
+        |> redirect(to: ~p"/")
+      {:ok, %{status: 400}} ->
+        conn
+        |> put_flash(:error, "Error loging in")
+        |> redirect(to: ~p"/")
+      {:error, _reason} ->
+        conn
+        |> put_flash(:error, "An error occurred")
+        |> redirect(to: ~p"/")
+    end
+  end
+end
--- config/runtime.exs
+++ config/runtime.exs
@@ -21,6 +21,10 @@ import Config
 # General configurations #
 ##########################
 
+config :ueberauth, Ueberauth.Strategy.Google.OAuth,
+  client_id: System.get_env("GOOGLE_CLIENT_ID"),
+  client_secret: System.get_env("GOOGLE_CLIENT_SECRET")
+
 ############################
 # App configuration: arena #
 ############################
