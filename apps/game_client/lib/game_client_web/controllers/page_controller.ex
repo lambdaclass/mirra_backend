dc3b271d22920741e235c9b95d2cae90dac8316b
Amin Arria <arria.amin@gmail.com>
2024-05-03 19:35:37 +0200

    Fix user_id missing

--- apps/game_client/lib/game_client_web/controllers/page_controller.ex
+++ apps/game_client/lib/game_client_web/controllers/page_controller.ex
@@ -2,12 +2,12 @@ defmodule GameClientWeb.PageController do
   use GameClientWeb, :controller
 
   def home(conn, _params) do
-    user_id = get_session(conn, :user_id)
-    render(conn, :home, user_id: user_id)
+    current_user_id = get_session(conn, :user_id)
+    user_id = current_user_id || Ecto.UUID.generate()
+    render(conn, :home, current_user_id: current_user_id, user_id: user_id)
   end
 
-  def select_character(conn, %{"character" => character, "game_mode" => game_mode} = params) do
-    user_id = params["user_id"] || Ecto.UUID.generate()
+  def select_character(conn, %{"user_id" => user_id, "character" => character, "game_mode" => game_mode}) do
     redirect(conn, to: ~p"/board/#{user_id}/#{character}/player_name/#{game_mode}")
   end
 end
