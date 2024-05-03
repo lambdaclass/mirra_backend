3a517261e7134d89378cf43f61752504636a5acd
agustinesco <agustin.escobar@lambdaclass.com>
2024-05-03 13:41:37 -0300

    Replace amount validation

--- apps/gateway/lib/gateway/controllers/arena/currency_controller.ex
+++ apps/gateway/lib/gateway/controllers/arena/currency_controller.ex
@@ -9,13 +9,9 @@ defmodule Gateway.Controllers.Users.Currency.CurrencyController do
   """
 
   def modify_currency(conn, %{"currency_name" => currencty_name, "amount" => amount, "user_id" => user_id}) do
-    with {:value, {amount, _}} <- {:value, Integer.parse(amount)},
-         {:add_currency, {:ok, %UserCurrency{}}} <-
-           {:add_currency, Currencies.add_currency_by_name(user_id, currencty_name, amount)} do
-      send_resp(conn, 200, "Currency added")
-    else
-      {:value, :error} -> send_resp(conn, 400, "Invalid value")
-      {:add_currency, nil} -> send_resp(conn, 400, "Currency not found")
+    case Currencies.add_currency_by_name(user_id, currencty_name, amount) do
+      {:ok, %UserCurrency{}} -> send_resp(conn, 200, "Currency added")
+      _ -> send_resp(conn, 400, "Couldn't add currency")
     end
   end
 end
