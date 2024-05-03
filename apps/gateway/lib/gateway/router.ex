7a0c57e3bee073cd5d25592c078c47ee7e6985f4
agustinesco <agustin.escobar@lambdaclass.com>
2024-05-03 11:27:56 -0300

    Add currency endpoint

--- apps/gateway/lib/gateway/router.ex
+++ apps/gateway/lib/gateway/router.ex
@@ -9,6 +9,12 @@ defmodule Gateway.Router do
     pipe_through :api
   end
 
+  scope "/arena", Gateway.Controllers.Arena do
+    pipe_through :api
+
+    put "/currency/modify", CurrencyController, :modify_currency
+  end
+
   scope "/auth", Gateway do
     pipe_through :api
 
