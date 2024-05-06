e30368a73b2e85ae54ea9f418e91784a42f663fc
agustinesco <agustin.escobar@lambdaclass.com>
2024-05-06 19:06:37 -0300

    Add thropies depending on position

--- apps/game_backend/lib/game_backend/matches.ex
+++ apps/game_backend/lib/game_backend/matches.ex
@@ -2,21 +2,48 @@ defmodule GameBackend.Matches do
   @moduledoc """
   Matches
   """
+  alias GameBackend.Users
+  alias GameBackend.Users.Currencies
   alias Ecto.Multi
   alias GameBackend.Repo
   alias GameBackend.Matches.ArenaMatchResult
 
-  def create_arena_match_results(match_id, results) do
-    transaction = Multi.new()
+  def create_arena_match_results(results) do
+    {:ok, conrrency_config_json} =
+      Application.app_dir(:game_backend, "priv/currencies_rules.json")
+      |> File.read()
+
+    currency_config =
+      Jason.decode!(conrrency_config_json)
 
     transaction =
-      Enum.reduce(results, transaction, fn result, transaction_acc ->
-        attrs = Map.put(result, "match_id", match_id)
-        changeset = ArenaMatchResult.changeset(%ArenaMatchResult{}, attrs)
-        transaction_acc = Multi.insert(transaction_acc, {:insert, attrs["user_id"]}, changeset)
-        transaction_acc
+      Enum.reduce(results, Multi.new(), fn result, transaction_acc ->
+        changeset = ArenaMatchResult.changeset(%ArenaMatchResult{}, result)
+
+        amount_of_trophies = Currencies.get_amount_of_currency_by_name(result["user_id"], "Trophies")
+
+        {:ok, google_user} = Users.get_google_user(result["user_id"])
+
+        amount =
+          get_amount_of_trophies_to_modify(amount_of_trophies, result["position"], currency_config)
+
+        Multi.insert(transaction_acc, {:insert, result["user_id"]}, changeset)
+        |> Multi.run(
+          {:add_trophies_to, result["user_id"]},
+          fn _, _ ->
+            Currencies.add_currency_by_name!(google_user.user.id, "Trophies", amount)
+          end
+        )
       end)
 
     Repo.transaction(transaction)
+  end
+
+  def get_amount_of_trophies_to_modify(current_trophies, position, currencies_config) do
+    Enum.sort_by(currencies_config["rank"], fn %{"maximum_rank" => maximum} -> maximum end, :asc)
+    |> Enum.find(fn %{"maximum_rank" => maximum} ->
+      maximum > current_trophies
+    end)
+    |> Map.get(position)
   end
 end
