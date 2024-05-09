057ffff26f3ff7c0936afcab6ede4b0cda6d9e31
agustinesco <agustin.escobar@lambdaclass.com>
2024-05-09 18:27:04 -0300

    Use user in with instead of crashing

--- apps/gateway/lib/gateway/controllers/arena/currency_controller.ex
+++ apps/gateway/lib/gateway/controllers/arena/currency_controller.ex
@@ -1,6 +1,6 @@
 defmodule Gateway.Curse.Controllers.Users.CurrencyController do
   @moduledoc """
-  Controller to control currency changes in users
+    Controller to control currency changes in users
   """
 
   use Gateway, :controller
@@ -14,14 +14,15 @@ defmodule Gateway.Curse.Controllers.Users.CurrencyController do
         "amount" => amount,
         "user_id" => user_id
       }) do
-    {:ok, user} = Users.get_user(user_id)
     game_id = Utils.get_game_id(:curse_of_mirra)
 
-    with {:curse_user, ^game_id} <- {:curse_user, user.game_id},
+    with {:get_user, {:ok, user}} <- {:get_user, Users.get_user(user_id)},
+         {:curse_user, ^game_id} <- {:curse_user, user.game_id},
          {:add_currency, {:ok, user_currency}} <-
            {:add_currency, Currencies.add_currency_by_name_and_game(user_id, currencty_name, user.game_id, amount)} do
       send_resp(conn, 200, Jason.encode!(Map.take(user_currency, [:amount, :user_id, :currency_id])))
     else
+      {:get_user, _} -> send_resp(conn, 400, "User not found")
       {:curse_user, _} -> send_resp(conn, 400, "User from another game")
       {:add_currency, _} -> send_resp(conn, 400, "Couldn't add currency")
     end
