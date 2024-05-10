7525fe0e66b41127978b5e668df797ff0da4fd02
tvillegas98 <tvillegas@fi.uba.ar>
2024-05-10 15:26:40 -0300

    Refactor item logic to return the game state

--- apps/arena/lib/arena/game/item.ex
+++ apps/arena/lib/arena/game/item.ex
@@ -22,38 +22,51 @@ defmodule Arena.Game.Item do
     last_id = game_state.last_id + 1
     entity_player_owner = get_entity_player_owner(game_state, entity)
 
-    bomb_item_params = %{
+    new_item_params = %{
       name: mechanic_params.name,
       radius: 200.0,
       effects: [],
       on_pickup_effects: %{
         damage: %{
-          damage: mechanic_params.damage
-        }
+          damage_type: mechanic_params.damage_type,
+          damage_delay_ms: mechanic_params.damage_delay_ms
+        },
       },
-      mechanics: []
+      mechanics: %{}
     }
 
-    bomb_item = Entities.new_item(last_id, entity_player_owner.position, bomb_item_params)
+    bomb_item = Entities.new_item(last_id, entity_player_owner.position, new_item_params)
 
     game_state
     |> put_in([:last_id], last_id)
     |> put_in([:items, bomb_item.id], bomb_item)
   end
 
-  def do_pickup_effect(entity, item, pick_effects) when map_size(pick_effects) == 0 do
-    put_in(entity, [:aditional_info, :inventory], item)
+  def do_pickup_effect(game_state, entity, item, pick_effects) when map_size(pick_effects) == 0 do
+    entity = put_in(entity, [:aditional_info, :inventory], item)
+    put_in(game_state, [:players, entity.id], entity)
   end
 
-  def do_pickup_effect(entity, item, pickup_effects) when is_map(pickup_effects) do
+  def do_pickup_effect(game_state, entity, item, pickup_effects) when is_map(pickup_effects) do
     Enum.reduce(pickup_effects, entity, fn pick_up_effect, entity ->
-      do_pickup_effect(entity, item, pick_up_effect)
+      do_pickup_effect(game_state, entity, item, pick_up_effect)
     end)
   end
 
-  def do_pickup_effect(entity, _item, {:damage, pickup_effect_params}) do
-    Player.take_damage(entity, pickup_effect_params.damage)
+  def do_pickup_effect(game_state, entity, _item, {:damage, pickup_effect_params}) do
+    maybe_delay_damage(game_state, entity, pickup_effect_params)
   end
+
+  defp maybe_delay_damage(game_state, entity, %{damage_delay_ms: damage_delay_ms} = pickup_effect_params) do
+    Enum.reduce(pickup_effect_params.damage_type, game_state, fn damage_type, game_state ->
+      Arena.Game.Skill.do_mechanic(game_state, entity, damage_type, %{})
+    end)
+  end
+
+  defp maybe_delay_damage(game_state, entity, pickup_effect_params) do
+    Arena.Game.Skill.do_mechanic(game_state, entity, pickup_effect_params.damage_type, %{})
+  end
+
 
   defp get_entity_player_owner(_game_state, %{category: :player} = player), do: player
 
