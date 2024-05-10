b77f9e92571a36e172e59bb9170bd028ec7d3c03
agustinesco <agustin.escobar@lambdaclass.com>
2024-05-10 10:19:35 -0300

    Add quests tables

---
+++ apps/game_backend/priv/repo/migrations/20240509225708_add_quest_description_table.exs
@@ -0,0 +1,11 @@
+defmodule GameBackend.Repo.Migrations.AddQuestDescriptionTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:quest_descriptions) do
+      add :description, :string, default: ""
+      add :quest_objectives, {:array, :map}
+      timestamps()
+    end
+  end
+end
