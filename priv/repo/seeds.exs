4451edde3ee0e2627566f820d62ae573471e9246
Agustin Escobar <106101218+agustinesco@users.noreply.github.com>
2024-05-10 14:05:55 -0300

    [GH-565] add trophies currency (#568)
    * Create ArenaMatchResult schema
    
    * Endpoint to save matches
    
    * Modify match result endpoint to accept batch instead of single
    
    * Save arena game results when it ends
    
    * Differentiatie between user clients and bot clients for result reporting
    
    * Add match_id field to ArenaMatchResult
    
    * Formatting
    
    * Gateway AuthController.validate_token/2 return user details as well
    
    * Add ueberauth_google to game_client application and use Gateway to verify user
    
    * Use and pass along user_id in game_client to arena match
    
    * Various fixes
    
    * Formatting
    
    * Fix user_id missing
    
    * Add position calculation
    
    * Add currencies rules json
    
    * Create currency module
    
    * Delete unused module
    
    * Fix position sending
    
    * Add thropies depending on position
    
    * Add google user getter by id
    
    * Fix create method
    
    * Add trophies to seeds
    
    * Add position to arena match result
    
    * Fix amount of trophies fetch
    
    * Move config read to runtime configuration
    
    * Move clients needed from game launcher to config file
    
    * Move infinite rank to find default instead of case
    
    * Replace hardcoded numbers with utils function
    
    * Fix typo con rrency
    
    * Remove unused variable
    
    * Replace cliets with players in config
    
    * Use correct methos to add currencies
    
    ---------
    
    Co-authored-by: Amin Arria <arria.amin@gmail.com>

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,3 +1,4 @@
+alias GameBackend.Utils
 alias GameBackend.Campaigns
 alias GameBackend.Campaigns.Campaign
 alias GameBackend.Campaigns.Level
@@ -10,8 +11,8 @@ alias GameBackend.Units.Unit
 alias GameBackend.Users
 alias GameBackend.Users.KalineTreeLevel
 
-curse_of_mirra_id = 1
-champions_of_mirra_id = 2
+curse_of_mirra_id = Utils.get_game_id(:curse_of_mirra)
+champions_of_mirra_id = Utils.get_game_id(:champions_of_mirra)
 units_per_level = 5
 
 {:ok, _skills} = Champions.Config.import_skill_config()
@@ -59,6 +60,9 @@ units_per_level = 5
 
 {:ok, _fertilizer_currency} =
   Users.Currencies.insert_currency(%{game_id: champions_of_mirra_id, name: "Fertilizer"})
+
+{:ok, _trophies_currency} =
+  Users.Currencies.insert_currency(%{game_id: curse_of_mirra_id, name: "Trophies"})
 
 {:ok, _items} = Champions.Config.import_item_template_config()
 
