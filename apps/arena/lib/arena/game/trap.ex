d38c9eec794a31f41a9c0ce63f4ceafcd90cec3e
tvillegas98 <tvillegas@fi.uba.ar>
2024-05-14 16:16:51 -0300

    Add trap module

---
+++ apps/arena/lib/arena/game/trap.ex
@@ -0,0 +1,23 @@
+defmodule Arena.Game.Trap do
+  @doc """
+  Module to handle traps logic
+  """
+  alias Arena.Game.Skill
+
+  @doc """
+  Apply all trap mechanics to an entity
+  """
+  def do_mechanics(game_state, entity, mechanics) do
+    Enum.reduce(mechanics, game_state, fn mechanic, game_state_acc ->
+      do_mechanic(game_state_acc, entity, mechanic)
+    end)
+  end
+
+  @doc """
+  Apply a trap mechanic to an entity, depending on the mechanic type.
+  """
+  def do_mechanic(game_state, entity, {:circle_hit, circle_hit}) do
+    # We will be using the skill mechanic here until we abstract the attacks
+    Skill.do_mechanic(game_state, entity, {:circle_hit, circle_hit}, %{})
+  end
+end
