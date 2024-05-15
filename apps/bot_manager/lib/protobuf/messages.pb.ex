3782480872fa3e0cc0945b00f742b851fd6656ed
Nicolas Sanchez <sanchez.nicolas96@gmail.com>
2024-05-15 19:33:41 -0300

    Remove unwanted crate field in proto file

--- apps/bot_manager/lib/protobuf/messages.pb.ex
+++ apps/bot_manager/lib/protobuf/messages.pb.ex
@@ -496,7 +496,6 @@ defmodule BotManager.Protobuf.Crate do
   field(:health, 1, type: :uint64)
   field(:amount_of_power_ups, 2, type: :uint64, json_name: "amountOfPowerUps")
   field(:status, 3, type: BotManager.Protobuf.CrateStatus, enum: true)
-  field(:effects, 4, repeated: true, type: BotManager.Protobuf.Effect)
 end
 
 defmodule BotManager.Protobuf.Pool do
