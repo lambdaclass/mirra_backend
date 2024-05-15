15f17f51c127865e5c9cbc7ef8c20af8f3c567bf
tvillegas98 <tvillegas@fi.uba.ar>
2024-05-15 12:00:57 -0300

    Remove not used function in item.ex

--- apps/arena/lib/arena/game/item.ex
+++ apps/arena/lib/arena/game/item.ex
@@ -19,12 +19,11 @@ defmodule Arena.Game.Item do
   """
   def do_mechanic(game_state, entity, {:spawn_bomb, bomb_params}) do
     last_id = game_state.last_id + 1
-    entity_player_owner = get_entity_player_owner(game_state, entity)
 
     now = System.monotonic_time(:millisecond)
 
     new_trap =
-      Entities.new_trap(last_id, entity_player_owner.id, entity_player_owner.position, bomb_params)
+      Entities.new_trap(last_id, entity.id, entity.position, bomb_params)
       |> Map.put(:activate_at, now + bomb_params.activation_delay_ms)
       # TODO: This could depend on the animation time
       |> Map.put(:remove_at, now + bomb_params.activation_delay_ms * 2)
@@ -33,6 +32,4 @@ defmodule Arena.Game.Item do
     |> put_in([:last_id], last_id)
     |> put_in([:traps, new_trap.id], new_trap)
   end
-
-  defp get_entity_player_owner(_game_state, %{category: :player} = player), do: player
 end
