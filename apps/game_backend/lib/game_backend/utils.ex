4a95e7bf97954f002872105cf8d2b3eacc76884f
Nicolás Sanchez <sanchez.nicolas96@gmail.com>
2024-05-17 15:02:54 -0300

    [GH-583] Add daily rewards (#602)
    * Add functions for daily rewards logic
    
    * Add daily rewards business rules json and config
    
    * Add daily rewards fields to user table and schema
    
    * Add endpoint to claim current daily reward
    
    * Rename daily rewards fields
    
    * Create Utils function to get the daily rewards config
    
    * Fix bugs for 1st lifetime daily reward claiming
    
    * Create FallbackController to handle errors processing requests and sending back the proper response
    
    * Add new endpoint to get the user daily reward status
    
    * Fix user_in_daily_rewards_streak? function
    
    * Fix wrongly solved merge
    
    * Make daily rewards rules json more understandable and configurable
    
    * Fix user_can_claim function
    
    * Fix mark daily rewards as complete function
    
    * Add day to rewards map to know to what day it belongs
    
    * Insert correspond currencies when the user claims the daily reward
    
    * Add daily rewards config docs
    
    * Add daily rewards config inside game_backend config
    
    * Reorder routes and controllers to new curse_of_mirra folder
    
    * Move transactions from with clause to a Multi.transaction
    
    * Add new errors callbacks in fallback_controller

--- apps/game_backend/lib/game_backend/utils.ex
+++ apps/game_backend/lib/game_backend/utils.ex
@@ -5,4 +5,7 @@ defmodule GameBackend.Utils do
 
   def get_game_id(:curse_of_mirra), do: 1
   def get_game_id(:champions_of_mirra), do: 2
+
+  def get_daily_rewards_config(),
+    do: Application.get_env(:game_backend, :daily_rewards_config) |> Map.get("reward_per_day")
 end
---
+++ apps/gateway/lib/gateway/controllers/curse_of_mirra/user_controller.ex
@@ -0,0 +1,30 @@
+defmodule Gateway.Controllers.CurseOfMirra.UserController do
+  @moduledoc """
+  Controller for CurseOfMirra.User modifications.
+  """
+  use Gateway, :controller
+  alias GameBackend.Users
+  alias GameBackend.Rewards
+  alias GameBackend.Utils
+
+  action_fallback Gateway.Controllers.FallbackController
+
+  def claim_daily_reward(conn, %{"user_id" => user_id}) do
+    with {:ok, user} <- Users.get_user(user_id),
+         {:ok, :can_claim} <- Rewards.user_can_claim(user),
+         {:ok, daily_reward} <- Rewards.claim_daily_reward(user),
+         {:ok, _user_updates_map} <- Rewards.update_user_due_to_daily_rewards_claim(user, daily_reward) do
+      send_resp(conn, 200, Jason.encode!(user.id))
+    end
+  end
+
+  def get_daily_reward_status(conn, %{"user_id" => user_id}) do
+    with {:ok, user} <- Users.get_user(user_id) do
+      user_daily_reward_status =
+        Utils.get_daily_rewards_config()
+        |> Rewards.mark_user_daily_rewards_as_completed(user)
+
+      send_resp(conn, 200, Jason.encode!(user_daily_reward_status))
+    end
+  end
+end
