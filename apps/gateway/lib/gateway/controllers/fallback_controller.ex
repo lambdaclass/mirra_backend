5218f76aa90cd6543587bd4cf60dbb187612fb16
Nicolas Sanchez <sanchez.nicolas96@gmail.com>
2024-05-17 14:37:02 -0300

    Add new errors callbacks in fallback_controller

--- apps/gateway/lib/gateway/controllers/fallback_controller.ex
+++ apps/gateway/lib/gateway/controllers/fallback_controller.ex
@@ -19,4 +19,12 @@ defmodule Gateway.Controllers.FallbackController do
   def call(conn, {:error, :invalid_reward}) do
     send_resp(conn, 400, Jason.encode!(%{"error" => "invalid reward"}))
   end
+
+  def call(conn, {:error, failed_operation, _failed_value, _changes_so_far}) when is_binary(failed_operation) do
+    send_resp(conn, 400, Jason.encode!(%{"error" => failed_operation}))
+  end
+
+  def call(conn, {:error, failed_operation, _failed_value, _changes_so_far}) do
+    send_resp(conn, 400, Jason.encode!(%{"error" => Atom.to_string(failed_operation)}))
+  end
 end
