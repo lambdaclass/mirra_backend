a5de5cc79fcb7f0ebba69d19bc46d7269082b189
tvillegas98 <tvillegas@fi.uba.ar>
2024-05-23 12:19:46 -0300

    Handle edit for games

--- apps/configurator/lib/configurator_web/controllers/configuration_live/configuration.ex
+++ apps/configurator/lib/configurator_web/controllers/configuration_live/configuration.ex
@@ -19,50 +19,33 @@ defmodule ConfiguratorWeb.ConfigurationLive.Configuration do
     {:ok, socket}
   end
 
+  def handle_event("select_key", %{"key" => key}, socket) do
+    {:noreply, assign(socket, :selected_key, key)}
+  end
+
+  def handle_event("validate", %{"_target" => ["game", field], "game" => game} = params, socket) do
+    data = put_in(socket.assigns.data, ["game", field], game[field])
+    socket = assign(socket, :data, data)
+    {:noreply, socket}
+  end
+
+  def handle_event("save", params, socket) do
+    {:noreply, socket}
+  end
+
   defp process_keys(json) do
     Enum.reduce(json, [], fn {key, _v}, acc ->
       [key | acc]
     end)
   end
 
-  def handle_event("select_key", %{"key" => key}, socket) do
-    {:noreply, assign(socket, :selected_key, key)}
-  end
-
-  def render_field(assigns) when is_map(assigns) do
+  def input(assigns) do
     ~H"""
-    <%= for {key, value} <- assigns do %>
-      <%= render_field(value) %>
-    <% end %>
-    """
-  end
-
-  def render_field(assigns) when is_list(assigns) do
-    ~H"""
-    <%= for field <- assigns do %>
-      <%= render_field(field) %>
-    <% end %>
-    """
-  end
-
-  def render_field(assigns) when is_integer(assigns) or is_float(assigns) do
-    ~H"""
-    <input type="number" value={assigns} />
-    """
-  end
-
-  def render_field(assigns) when is_boolean(assigns) do
-    ~H"""
-    <input type="checkbox" checked={assigns} />
-    """
-  end
-
-  def render_field(assigns) when is_binary(assigns) do
-    ~H"""
-    <input type="text" value={assigns} />
+    <input id={@field.id} name={@field.name} value={@field.value} type={@type} label={@label} />
     """
   end
 
   def type(type) when is_integer(type) or is_float(type), do: "number"
+  def type(type) when is_boolean(type), do: "checkbox"
   def type(_type), do: "text"
 end
