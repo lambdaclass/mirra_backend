e381f570566e3dcb2f9a1c8dc4d9c90fe0d205c0
Nicolas Sanchez <sanchez.nicolas96@gmail.com>
2024-05-24 19:32:28 -0300

    Add new StoreController to handle Store requests and Store module for its behavior

---
+++ apps/game_backend/lib/game_backend/stores.ex
@@ -0,0 +1,51 @@
+defmodule GameBackend.Stores do
+  @moduledoc """
+  Store operations.
+  """
+  alias GameBackend.Stores.Store
+  alias GameBackend.Repo
+
+  @doc """
+  Inserts a Store.
+  """
+  def insert_store(attrs) do
+    %Store{}
+    |> Store.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  @doc """
+  Get Store by name.
+  """
+  def get_store_by_name(name) do
+    case Repo.get_by(Store, name: name) do
+      nil -> {:error, :not_found}
+      store -> {:ok, store}
+    end
+  end
+
+  @doc """
+  Returns {:ok, :available} if we are within Store dates.
+  Returns {:error, :not_available} otherwise.
+  """
+  def is_available(store) do
+    now = DateTime.utc_now()
+
+    if (is_nil(store.start_date) or store.start_date <= now) and store.end_date >= now do
+      {:ok, :available}
+    else
+      {:error, :not_available}
+    end
+  end
+
+  def list_items_with_prices(store) do
+    store = Repo.preload(store, items: [item_costs: [currency_costs: :currency]])
+
+    Enum.flat_map(store.items, fn item ->
+      Enum.map(item.item_costs, fn item_cost ->
+        {item.name,
+         Enum.map(item_cost.currency_costs, fn currency_cost -> {currency_cost.currency.name, currency_cost.amount} end)}
+      end)
+    end)
+  end
+end
---
+++ apps/gateway/lib/gateway/controllers/curse_of_mirra/store_controller.ex
@@ -0,0 +1,36 @@
+defmodule Gateway.Controllers.CurseOfMirra.StoreController do
+  @moduledoc """
+  Controller for Item modifications.
+  """
+  use Gateway, :controller
+  alias GameBackend.Items
+  alias GameBackend.Utils
+  alias GameBackend.Users.Currencies
+  alias GameBackend.Stores
+
+  action_fallback Gateway.Controllers.FallbackController
+
+  def list_items(conn, params) do
+    with {:ok, store} <- Stores.get_store_by_name(params["store_name"]),
+         {:ok, items_with_prices} <- Stores.list_items_with_prices(store) do
+      send_resp(conn, 200, Jason.encode!(items_with_prices))
+    end
+  end
+
+  def buy_item(conn, params) do
+    with {:ok, store} <- Stores.get_store_by_name(params["store_name"]),
+         {:ok, :available} <- Stores.is_available(store),
+         {:ok, :item_in_store} <- Stores.item_in_store(params["item_name"], store),
+         {:ok, item_template_id} <-
+           Items.get_template_id_by_name_and_game_id(
+             params["item_name"],
+             Utils.get_game_id(:curse_of_mirra)
+           ),
+         {:ok, item_cost} <-
+           Items.get_item_cost_by_name(params["cost_name"], item_template_id),
+         {:can_afford, true} <- {:can_afford, Currencies.can_afford(params["user_id"], item_cost.currency_costs)},
+         {:ok, item_updates_map} <- Items.buy_item(params["user_id"], item_template_id, item_cost.currency_costs) do
+      send_resp(conn, 200, Jason.encode!(item_updates_map.item.id))
+    end
+  end
+end
