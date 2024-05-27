6baf65c35108c1789944fe6028b96813c186f25a
NContinanza <nicolas.continanza@lambdaclass.com>
2024-05-27 15:35:06 -0300

    Adapt tests to initial blueprints change

--- apps/champions/lib/champions/users.ex
+++ apps/champions/lib/champions/users.ex
@@ -137,7 +137,7 @@ defmodule Champions.Users do
     Currencies.add_currency(
       user.id,
       Currencies.get_currency_by_name_and_game!("Blueprints", Utils.get_game_id(:champions_of_mirra)).id,
-      100
+      50
     )
   end
 
