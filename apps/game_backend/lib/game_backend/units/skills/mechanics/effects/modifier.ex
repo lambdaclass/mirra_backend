c03047339f14b63ebfa70e0a4b6627a95cfa13e8
lotuuu <nicolas@lotufo.com.ar>
2024-06-07 16:27:41 -0300

    Add multiply_type field to modifier

--- apps/game_backend/lib/game_backend/units/skills/mechanics/effects/modifier.ex
+++ apps/game_backend/lib/game_backend/units/skills/mechanics/effects/modifier.ex
@@ -6,6 +6,7 @@ defmodule GameBackend.Units.Skills.Mechanics.Effects.Modifier do
   - `"Add"`: Adds the result to the Modifier's specified Attribute. Use a negative value for subtraction.
   - `"Multiply"`: Multiplies the result to the Modifier's specified Attribute. Use a value between 0 and 1 for division.
   - `"Override"`: Overrides the Modifier's specified Attribute with the result.
+  For `"Multiply"`, the `multiply_type` field is used to determine how the value is multiplied. It can be `"Additive"` or `"Multiplicative"`. The default is `"Additive"`. This value is ignored for `"Add"` and `"Override"` operations.
 
   The `CurrentValue` of an `Attribute` is the aggregate result of all of its `Modifiers` added to its `BaseValue`.
   The formula for how `Modifiers` are aggregated is defined as follows:
@@ -40,11 +41,12 @@ defmodule GameBackend.Units.Skills.Mechanics.Effects.Modifier do
     field(:attribute, :string)
     field(:operation, :string)
     field(:magnitude, :float)
+    field(:multiply_type, :string)
   end
 
   def changeset(modifier, attrs) do
     modifier
-    |> cast(attrs, [:attribute, :operation, :magnitude])
+    |> cast(attrs, [:attribute, :operation, :magnitude, :multiply_type])
     |> validate_inclusion(:operation, ~w[Add Multiply Override])
     |> validate_required([:operation, :magnitude, :attribute])
   end
