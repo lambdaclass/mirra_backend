7956eba5d21b569a92b364935fd34d5d6d5e368d
tvillegas98 <tvillegas@fi.uba.ar>
2024-06-12 00:49:07 -0300

    Remove no longer used function nested map level

--- apps/configurator/lib/configurator_web/controllers/utils_configuration.ex
+++ apps/configurator/lib/configurator_web/controllers/utils_configuration.ex
@@ -2,17 +2,6 @@ defmodule ConfiguratorWeb.UtilsConfiguration do
   @moduledoc """
   Module with utility functions for configuration data.
   """
-  def nested_maps_level(map, level) do
-    map
-    |> Enum.reduce(level, fn {_key, value}, level ->
-      if is_map(value) do
-        nested_maps_level(value, level + 1)
-      else
-        level
-      end
-    end)
-  end
-
   def key_prettier(key) do
     key
     |> String.split("_")
