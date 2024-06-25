782947df7012c24aa2b679c9235944f2a48835d0
tvillegas98 <tvillegas@fi.uba.ar>
2024-06-25 18:50:51 -0300

    Use Map.drop instead of delete

--- apps/configurator/lib/configurator_web/controllers/utils.ex
+++ apps/configurator/lib/configurator_web/controllers/utils.ex
@@ -6,11 +6,15 @@ defmodule ConfiguratorWeb.Utils do
   def transform_to_map_from_ecto_struct(ecto_struct) when is_struct(ecto_struct) do
     ecto_struct
     |> Map.from_struct()
-    |> Map.delete(:__meta__)
-    |> Map.delete(:__struct__)
-    |> Map.delete(:inserted_at)
-    |> Map.delete(:updated_at)
-    |> Map.delete(:id)
+    |> Map.drop(
+      [
+        :__meta__,
+        :__struct__,
+        :inserted_at,
+        :updated_at,
+        :id
+      ]
+    )
   end
 
   def transform_to_map_from_ecto_struct(ecto_structs) when is_list(ecto_structs) do
