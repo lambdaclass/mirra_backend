82598d92228b1544a9e56d14e0a74124889e03c1
Nicolas Sanchez <sanchez.nicolas96@gmail.com>
2024-06-26 16:00:31 -0300

    Differentiate characters table with configurator characters table

--- apps/configurator/priv/repo/migrations/20240626154341_delete_configurator_characters.exs
+++ apps/configurator/priv/repo/migrations/20240626154341_delete_configurator_characters.exs
@@ -2,6 +2,6 @@ defmodule Configurator.Repo.Migrations.DeleteConfiguratorCharacters do
   use Ecto.Migration
 
   def change do
-    drop table(:characters)
+    drop table(:characters_config)
   end
 end
