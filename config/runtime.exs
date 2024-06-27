eebb95de3ce3c263002cf9c01cdd063a0ef9411f
Nicolas Sanchez <sanchez.nicolas96@gmail.com>
2024-06-27 19:07:23 -0300

    Start configurator webserver in another port

--- config/runtime.exs
+++ config/runtime.exs
@@ -340,8 +340,8 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "example.com"
-  port = String.to_integer(System.get_env("PORT") || "4100")
+  host = System.get_env("CONFIGURATOR_HOST") || "example.com"
+  port = String.to_integer(System.get_env("CONFIGURATOR_PORT") || "4100")
 
   config :configurator, ConfiguratorWeb.Endpoint,
     url: [host: host, port: port, scheme: "https"],
