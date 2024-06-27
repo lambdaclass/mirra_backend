7e510c23ac5912b303940f7dee2af30f54443420
Nicolas Sanchez <sanchez.nicolas96@gmail.com>
2024-06-27 20:38:59 -0300

    Update configurator host and port

--- config/runtime.exs
+++ config/runtime.exs
@@ -344,15 +344,8 @@ if config_env() == :prod do
   port = String.to_integer(System.get_env("CONFIGURATOR_PORT") || "4100")
 
   config :configurator, ConfiguratorWeb.Endpoint,
-    url: [host: host, port: port, scheme: "https"],
-    http: [
-      # Enable IPv6 and bind on all interfaces.
-      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
-      # See the documentation on https://hexdocs.pm/bandit/Bandit.html#t:options/0
-      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: port
-    ],
+    url: [host: host, port: port],
+    http: [ip: {127, 0, 0, 1}, port: port],
     secret_key_base: secret_key_base
 
   # ## SSL Support
