9397c57edd0d71c4a2f270bc02d63cd7025cb831
tvillegas98 <tvillegas@fi.uba.ar>
2024-07-02 10:09:52 -0300

    Move transform_ecto_struct_to_map to GameBackend.Utils

--- apps/gateway/lib/gateway/controllers/curse_of_mirra/configuration_controller.ex
+++ apps/gateway/lib/gateway/controllers/curse_of_mirra/configuration_controller.ex
@@ -3,40 +3,33 @@ defmodule Gateway.Controllers.CurseOfMirra.ConfigurationController do
   Controller for Curse of Mirra Configurations
   """
   use Gateway, :controller
-  alias GameBackend.CurseOfMirra.Config
   alias GameBackend.Configuration
+  alias GameBackend.Utils
 
   action_fallback Gateway.Controllers.FallbackController
 
   def get_characters_configuration(conn, _params) do
-    case Config.get_characters_config() do
-      nil -> {:error, :not_found}
-      characters_config -> send_resp(conn, 200, Jason.encode!(characters_config))
+    case GameBackend.Units.Characters.get_curse_characters() do
+      [] ->
+        {:error, :not_found}
+
+      characters ->
+        send_resp(
+          conn,
+          200,
+          Jason.encode!(
+            Utils.transform_to_map_from_ecto_struct(characters, [
+              :basic_skill,
+              :ultimate_skill
+            ])
+          )
+        )
     end
   end
 
   def get_game_configuration(conn, _params) do
-    game_configuration = Configuration.get_latest_game_configuration() |> transform_to_map_from_ecto_struct()
+    game_configuration = Configuration.get_latest_game_configuration()
+    game_configuration = Utils.transform_to_map_from_ecto_struct(game_configuration)
     send_resp(conn, 200, Jason.encode!(game_configuration))
-  end
-
-  defp transform_to_map_from_ecto_struct(ecto_struct) when is_struct(ecto_struct) do
-    ecto_struct
-    |> Map.from_struct()
-    |> Map.drop([
-      :__meta__,
-      :__struct__,
-      :inserted_at,
-      :updated_at,
-      :id,
-      :basic_skill,
-      :ultimate_skill
-    ])
-  end
-
-  defp transform_to_map_from_ecto_struct(ecto_structs) when is_list(ecto_structs) do
-    Enum.map(ecto_structs, fn ecto_struct ->
-      transform_to_map_from_ecto_struct(ecto_struct)
-    end)
   end
 end
