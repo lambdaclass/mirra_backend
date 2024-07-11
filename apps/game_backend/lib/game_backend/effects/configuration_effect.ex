933b17626ce063083b9d1c7455c211580dfceb6a
tvillegas98 <tvillegas@fi.uba.ar>
2024-07-11 17:44:23 -0300

    Add consumable item id to configuration effect

--- apps/game_backend/lib/game_backend/effects/configuration_effect.ex
+++ apps/game_backend/lib/game_backend/effects/configuration_effect.ex
@@ -9,7 +9,7 @@ defmodule GameBackend.Effects.ConfigurationEffect do
     field(:remove_on_action, :boolean, default: false)
     field(:one_time_application, :boolean, default: false)
 
-    belongs_to(:effect, ConsumableItem)
+    belongs_to(:consumable_item, ConsumableItem)
 
     timestamps(type: :utc_datetime)
   end
@@ -17,7 +17,7 @@ defmodule GameBackend.Effects.ConfigurationEffect do
   @doc false
   def changeset(configuration_effect, attrs) do
     configuration_effect
-    |> cast(attrs, [:name, :duration_ms, :remove_on_action, :one_time_application])
-    |> validate_required([:name, :duration_ms, :remove_on_action, :one_time_application])
+    |> cast(attrs, [:name, :duration_ms, :remove_on_action, :one_time_application, :consumable_item_id])
+    |> validate_required([:name, :duration_ms, :remove_on_action, :one_time_application, :consumable_item_id])
   end
 end
