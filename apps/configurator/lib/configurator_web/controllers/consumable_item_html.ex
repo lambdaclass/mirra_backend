ebab59411feade3292214c997e15be5922699475
tvillegas98 <tvillegas@fi.uba.ar>
2024-07-11 17:45:04 -0300

    Add modal for effects in consumable item index

--- apps/configurator/lib/configurator_web/controllers/consumable_item_html.ex
+++ apps/configurator/lib/configurator_web/controllers/consumable_item_html.ex
@@ -10,4 +10,18 @@ defmodule ConfiguratorWeb.ConsumableItemHTML do
   attr :action, :string, required: true
 
   def consumable_item_form(assigns)
+
+  def display_effect(assigns) do
+    ~H"""
+    <.modal id={@consumable_item.name}>
+      <.table id="effects" rows={@consumable_item.effects}>
+        <:col :let={effect} label="Name"><%= effect.name %></:col>
+        <:col :let={effect} label="Remove on Action"><%= effect.remove_on_action %></:col>
+        <:col :let={effect} label="Duration ms"><%= effect.duration_ms %></:col>
+        <:col :let={effect} label="One Time Application"><%= effect.one_time_application %></:col>
+      </.table>
+    </.modal>
+    <.button class="" phx-click={show_modal(@consumable_item.name)}>Effects</.button>
+    """
+  end
 end
