3de32d0e94d6bcb1f3462625daaf48bc99b6ea78
tvillegas98 <tvillegas@fi.uba.ar>
2024-07-11 15:37:53 -0300

    Use new effect schema in consumable items and add effects to seeds

--- apps/game_backend/lib/game_backend/items/consumable_item.ex
+++ apps/game_backend/lib/game_backend/items/consumable_item.ex
@@ -2,10 +2,13 @@ defmodule GameBackend.Items.ConsumableItem do
   use GameBackend.Schema
   import Ecto.Changeset
 
+  alias GameBackend.Effects.ConfigurationEffect
+
   schema "consumable_items" do
     field(:name, :string)
     field(:radius, :float)
-    field(:effects, {:array, :string})
+
+    has_many(:effects, ConfigurationEffect)
 
     timestamps(type: :utc_datetime)
   end
@@ -13,7 +16,8 @@ defmodule GameBackend.Items.ConsumableItem do
   @doc false
   def changeset(consumable_item, attrs) do
     consumable_item
-    |> cast(attrs, [:name, :radius, :effects])
-    |> validate_required([:name, :radius, :effects])
+    |> cast(attrs, [:name, :radius])
+    |> cast_assoc(:effects)
+    |> validate_required([:name, :radius])
   end
 end
