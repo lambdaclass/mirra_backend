d319835bc93de2d03429c6cd2bc4428a6c84a803
tvillegas98 <tvillegas@fi.uba.ar>
2024-07-12 16:20:39 -0300

    Add edit handle in consumable items liveview form

--- apps/configurator/lib/configurator_web/live/consumable_items/form.ex
+++ apps/configurator/lib/configurator_web/live/consumable_items/form.ex
@@ -7,6 +7,19 @@ defmodule ConfiguratorWeb.ConsumableItemsLive.Form do
 
   def mount(
         _params,
+        %{"consumable_item" => consumable_item},
+        socket
+      ) do
+    changeset = Items.change_consumable_item(consumable_item) |> IO.inspect()
+
+    socket =
+      socket |> assign(:changeset, changeset) |> assign(:action, "update") |> assign(:consumable_item, consumable_item)
+
+    {:ok, socket}
+  end
+
+  def mount(
+        _params,
         _session,
         socket
       ) do
@@ -37,6 +50,25 @@ defmodule ConfiguratorWeb.ConsumableItemsLive.Form do
         {:ok, consumable_item} ->
           socket
           |> put_flash(:info, "Consumable item created successfully.")
+          |> redirect(to: ~p"/consumable_items/#{consumable_item.id}")
+
+        {:error, %Ecto.Changeset{} = changeset} ->
+          socket
+          |> put_flash(:error, "Please correct the errors below.")
+          |> assign(:changeset, changeset)
+      end
+
+    {:noreply, socket}
+  end
+
+  def handle_event("update", %{"consumable_item" => consumable_item_params}, socket) do
+    consumable_item = socket.assigns.consumable_item
+
+    socket =
+      case Items.update_consumable_item(consumable_item, consumable_item_params) do
+        {:ok, consumable_item} ->
+          socket
+          |> put_flash(:info, "Consumable item updated successfully.")
           |> redirect(to: ~p"/consumable_items/#{consumable_item.id}")
 
         {:error, %Ecto.Changeset{} = changeset} ->
