e8d903ddfd188e736eb757d86616bf7d69431e64
tvillegas98 <tvillegas@fi.uba.ar>
2024-07-11 23:28:44 -0300

    Add liveview for consumable item forms

---
+++ apps/configurator/lib/configurator_web/live/consumable_items/form.ex
@@ -0,0 +1,50 @@
+defmodule ConfiguratorWeb.ConsumableItemsLive.Form do
+  use ConfiguratorWeb, :live_view
+
+  alias GameBackend.Items
+  alias GameBackend.Items.ConsumableItem
+  alias GameBackend.Effects.ConfigurationEffect
+
+  def mount(
+        _params,
+        _session,
+        socket
+      ) do
+    changeset = Items.change_consumable_item(%ConsumableItem{})
+    socket = socket |> assign(:changeset, changeset) |> assign(:action, "save")
+    {:ok, socket}
+  end
+
+  def handle_event("validate", %{"consumable_item" => consumable_item_params}, socket) do
+    changeset = socket.assigns.changeset
+    changeset = ConsumableItem.changeset(changeset, consumable_item_params)
+
+    {:noreply, assign(socket, :changeset, changeset)}
+  end
+
+  def handle_event("add_effect", _params, socket) do
+    changeset = socket.assigns.changeset
+    effects = Ecto.Changeset.get_field(changeset, :effects) || []
+    new_effect = %ConfigurationEffect{}
+    changeset = Ecto.Changeset.put_assoc(changeset, :effects, [new_effect | effects])
+
+    {:noreply, assign(socket, :changeset, changeset)}
+  end
+
+  def handle_event("save", %{"consumable_item" => consumable_item_params}, socket) do
+    socket =
+      case Items.create_consumable_item(consumable_item_params) |> IO.inspect() do
+        {:ok, consumable_item} ->
+          socket
+          |> put_flash(:info, "Consumable item created successfully.")
+          |> redirect(to: ~p"/consumable_items/#{consumable_item.id}")
+
+        {:error, %Ecto.Changeset{} = changeset} ->
+          socket
+          |> put_flash(:error, "Please correct the errors below.")
+          |> assign(:changeset, changeset)
+      end
+
+    {:noreply, socket}
+  end
+end
