36a561caecded092eba25a8c888397c90508b537
tvillegas98 <tvillegas@fi.uba.ar>
2024-07-12 18:24:42 -0300

    Add mechanics to configuration effects just to make it work

--- apps/game_backend/lib/game_backend/effects/configuration_effect.ex
+++ apps/game_backend/lib/game_backend/effects/configuration_effect.ex
@@ -3,12 +3,15 @@ defmodule GameBackend.Effects.ConfigurationEffect do
   import Ecto.Changeset
   alias GameBackend.Items.ConsumableItem
 
-  @derive {Jason.Encoder, only: [:name, :duration_ms, :remove_on_action, :one_time_application]}
+  @derive {Jason.Encoder, only: [:name, :duration_ms, :remove_on_action, :one_time_application, :mechanics]}
   schema "configuration_effects" do
     field(:name, :string)
     field(:duration_ms, :integer)
     field(:remove_on_action, :boolean, default: false)
     field(:one_time_application, :boolean, default: false)
+
+    ## TODO This relationship will be addressed in another issue: https://github.com/lambdaclass/mirra_backend/issues/771
+    field(:mechanics, {:map, :map})
 
     belongs_to(:consumable_item, ConsumableItem)
 
@@ -18,7 +21,7 @@ defmodule GameBackend.Effects.ConfigurationEffect do
   @doc false
   def changeset(configuration_effect, attrs) do
     configuration_effect
-    |> cast(attrs, [:name, :duration_ms, :remove_on_action, :one_time_application, :consumable_item_id])
+    |> cast(attrs, [:name, :duration_ms, :remove_on_action, :one_time_application, :consumable_item_id, :mechanics])
     |> validate_required([:name, :duration_ms, :remove_on_action, :one_time_application])
   end
 end
--- apps/game_backend/priv/repo/migrations/20240711165954_create_configuration_effects.exs
+++ apps/game_backend/priv/repo/migrations/20240711165954_create_configuration_effects.exs
@@ -9,6 +9,7 @@ defmodule Configurator.Repo.Migrations.CreateConfigurationEffects do
       add :one_time_application, :boolean, default: false, null: false
 
       add :consumable_item_id, references(:consumable_items, on_delete: :nothing)
+      add :mechanics, {:map, :string}
 
       timestamps(type: :utc_datetime)
     end
