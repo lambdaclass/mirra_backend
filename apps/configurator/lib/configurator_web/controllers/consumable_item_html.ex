0138f6fcc60affe40934b4bf1b990a4ecdc39f19
tvillegas98 <tvillegas@fi.uba.ar>
2024-07-15 18:48:48 -0300

    Show mechanics as a map for effects

--- apps/configurator/lib/configurator_web/controllers/consumable_item_html.ex
+++ apps/configurator/lib/configurator_web/controllers/consumable_item_html.ex
@@ -11,6 +11,7 @@ defmodule ConfiguratorWeb.ConsumableItemHTML do
         <:col :let={effect} label="Remove on Action"><%= effect.remove_on_action %></:col>
         <:col :let={effect} label="Duration ms"><%= effect.duration_ms %></:col>
         <:col :let={effect} label="One Time Application"><%= effect.one_time_application %></:col>
+        <:col :let={effect} label="Mechanics"><%= Jason.encode!(effect.mechanics) %></:col>
       </.table>
     </.modal>
     <.button class="" phx-click={show_modal(@consumable_item.name)}>Effects</.button>
