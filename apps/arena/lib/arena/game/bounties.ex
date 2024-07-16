392c78e22eaa3114ca087eb68eb69a23b99e7da7
agustinesco <agustinesco@outlook.es>
2024-07-16 15:11:35 -0300

    Anihilate to_atom and use custom function instead

--- apps/arena/lib/arena/game/bounties.ex
+++ apps/arena/lib/arena/game/bounties.ex
@@ -1,6 +1,9 @@
 defmodule Arena.Game.Bounties do
   @moduledoc """
   Module to handle quest logic in arena application
+  TODO this module shouldn't exist, this a workaround until we have a shared application to
+  share business logic between applications without deployed heavy ones
+  issue# https://github.com/lambdaclass/mirra_backend/issues/774
   """
 
   defp parse_comparator("equal"), do: &Kernel.==/2
@@ -23,9 +26,15 @@ defmodule Arena.Game.Bounties do
       arena_match_results
       |> filter_results_that_meet_quest_conditions(quest.conditions)
       |> Enum.reduce(0, fn arena_match_result, acc ->
-        type = String.to_atom(quest.objective.match_tracking_field)
+        arena_match_result_field_to_atom(quest.objective.match_tracking_field)
+        # We won't accumulate progress if the objective has a wrong field getter
+        |> case do
+          nil ->
+            acc
 
-        acc + accumulate_objective_progress_by_scope(quest.objective.scope, Map.get(arena_match_result, type))
+          type ->
+            acc + accumulate_objective_progress_by_scope(quest.objective.scope, Map.get(arena_match_result, type))
+        end
       end)
 
     comparator = parse_comparator(quest.objective.comparison)
@@ -36,14 +45,34 @@ defmodule Arena.Game.Bounties do
   defp filter_results_that_meet_quest_conditions(arena_match_results, conditions) do
     Enum.filter(arena_match_results, fn arena_match_result ->
       Enum.all?(conditions, fn condition ->
-        type = String.to_atom(condition.match_tracking_field)
-        value = condition.value
-        arena_match_result_value = Map.get(arena_match_result, type)
+        case arena_match_result_field_to_atom(condition.match_tracking_field) do
+          # We'll result in a false value in case that the quest condition has a wrong field getter
+          nil ->
+            false
 
-        comparator = parse_comparator(condition.comparison)
+          type ->
+            value = condition["value"]
+            arena_match_result_value = Map.get(arena_match_result, type)
 
-        comparator.(arena_match_result_value, value)
+            comparator = parse_comparator(condition.comparison)
+
+            comparator.(arena_match_result_value, value)
+        end
       end)
     end)
   end
+
+  defp arena_match_result_field_to_atom("result"), do: :result
+  defp arena_match_result_field_to_atom("kills"), do: :kills
+  defp arena_match_result_field_to_atom("deaths"), do: :deaths
+  defp arena_match_result_field_to_atom("character"), do: :character
+  defp arena_match_result_field_to_atom("match_id"), do: :match_id
+  defp arena_match_result_field_to_atom("user_id"), do: :user_id
+  defp arena_match_result_field_to_atom("position"), do: :position
+  defp arena_match_result_field_to_atom("damage_done"), do: :damage_done
+  defp arena_match_result_field_to_atom("damage_taken"), do: :damage_taken
+  defp arena_match_result_field_to_atom("health_healed"), do: :health_healed
+  defp arena_match_result_field_to_atom("killed_by_bot"), do: :killed_by_bot
+  defp arena_match_result_field_to_atom("duration_ms"), do: :duration_ms
+  defp arena_match_result_field_to_atom(_), do: nil
 end
