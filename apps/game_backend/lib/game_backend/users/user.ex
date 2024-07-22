2a61c2779cc2faf46d4c183f11edb995688d49c6
Agustin Escobar <106101218+agustinesco@users.noreply.github.com>
2024-07-22 13:08:20 -0300

    Update apps/game_backend/lib/game_backend/users/user.ex
    Co-authored-by: Nicolás Sanchez <sanchez.nicolas96@gmail.com>

--- apps/game_backend/lib/game_backend/users/user.ex
+++ apps/game_backend/lib/game_backend/users/user.ex
@@ -67,7 +67,7 @@ defmodule GameBackend.Users.User do
     |> cast_assoc(:unlocks)
     |> assoc_constraint(:google_user)
     |> validate_required([:game_id, :username])
-    |> validate_length(:username, min: 3, max: 9)
+    |> validate_length(:username, min: 2, max: 9)
     |> cast_assoc(:units, with: &GameBackend.Units.Unit.changeset/2)
   end
 
