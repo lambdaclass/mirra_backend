63015536a993324512f055ef632ea604b2616781
agustinesco <agustinesco@outlook.es>
2024-07-26 12:52:06 -0300

    Fix tests

--- apps/configurator/test/configurator_web/controllers/map_configuration_controller_test.exs
+++ apps/configurator/test/configurator_web/controllers/map_configuration_controller_test.exs
@@ -5,9 +5,9 @@ defmodule ConfiguratorWeb.MapConfigurationControllerTest do
   import Configurator.AccountsFixtures
   setup [:create_authenticated_conn]
 
-  @create_attrs %{radius: "120.5", initial_positions: "", obstacles: "", bushes: ""}
-  @update_attrs %{radius: "456.7", initial_positions: "", obstacles: "", bushes: ""}
-  @invalid_attrs %{radius: nil, initial_positions: nil, obstacles: nil, bushes: nil}
+  @create_attrs %{radius: "120.5", initial_positions: "", obstacles: "", bushes: "", pools: ""}
+  @update_attrs %{radius: "456.7", initial_positions: "", obstacles: "", bushes: "", pools: ""}
+  @invalid_attrs %{radius: nil, initial_positions: nil, obstacles: nil, bushes: nil, pools: nil}
 
   describe "index" do
     test "lists all map_configurations", %{conn: conn} do
