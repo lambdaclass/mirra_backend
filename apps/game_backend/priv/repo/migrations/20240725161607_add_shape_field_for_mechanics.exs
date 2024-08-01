b0e18e8934ee4a662daa2b1d19086e9df7fc133d
agustinesco <agustinesco@outlook.es>
2024-08-01 19:31:51 -0300

    Use execute/2 instead of up and down

--- apps/game_backend/priv/repo/migrations/20240725161607_add_shape_field_for_mechanics.exs
+++ apps/game_backend/priv/repo/migrations/20240725161607_add_shape_field_for_mechanics.exs
@@ -1,7 +1,7 @@
 defmodule GameBackend.Repo.Migrations.AddShapeFieldForMechanics do
   use Ecto.Migration
 
-  def up do
+  def change do
     alter table(:mechanics) do
       add :shape, :string
       add :vertices, {:array, :map}
@@ -11,13 +11,7 @@ defmodule GameBackend.Repo.Migrations.AddShapeFieldForMechanics do
     UPDATE mechanics mechanic
     SET shape = 'circle', vertices = '{}'::jsonb[]
     WHERE mechanic.type = 'spawn_pool';
-    """)
+    """, "")
   end
 
-  def down do
-    alter table(:mechanics) do
-      remove :shape, :string
-      remove :vertices, {:array, :map}
-    end
-  end
 end
