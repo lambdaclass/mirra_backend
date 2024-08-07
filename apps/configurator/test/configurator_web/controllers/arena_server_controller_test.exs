0761087b90676f4f5f77e96604e04403c0dfb92c
agustinesco <agustinesco@outlook.es>
2024-08-07 13:23:01 -0300

    Fix tests

--- apps/configurator/test/configurator_web/controllers/arena_server_controller_test.exs
+++ apps/configurator/test/configurator_web/controllers/arena_server_controller_test.exs
@@ -100,6 +100,7 @@ defmodule ConfiguratorWeb.ArenaServerControllerTest do
       conn
       |> Phoenix.ConnTest.init_test_session(%{})
       |> Plug.Conn.put_session(:user_token, token)
+      |> Plug.Conn.put_session(:current_user, user)
 
     %{conn: conn}
   end
