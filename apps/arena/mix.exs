1e0f8c4e0b8b10b63d0acb3c58dc8a93bce21581
agustinesco <agustinesco@outlook.es>
2024-08-07 19:16:15 -0300

    change arena version

--- apps/arena/mix.exs
+++ apps/arena/mix.exs
@@ -4,7 +4,7 @@ defmodule Arena.MixProject do
   def project do
     [
       app: :arena,
-      version: "0.1.14",
+      version: "0.2.14",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
