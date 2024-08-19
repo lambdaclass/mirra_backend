a6280855629d970b3b3ce92b5fba7cdcfe3c7e5e
Agustin Escobar <106101218+agustinesco@users.noreply.github.com>
2024-08-19 11:04:08 -0300

    [GH-842] Users screen information (#843)
    * Add highest historical prestige
    
    * Add user stats
    
    * Use subqueries instead of elixir
    
    * Update highest historical prestige on match end
    
    * Changes requested
    
    * Delete inexistant function
    
    * Fix credo complain
    
    * Fix most played character subquery
    
    * Fix unit default prestige
    
    * Fix credo complain 2

--- apps/game_backend/lib/game_backend/users/user.ex
+++ apps/game_backend/lib/game_backend/users/user.ex
@@ -20,7 +20,16 @@ defmodule GameBackend.Users.User do
     Unlock
   }
 
-  @derive {Jason.Encoder, only: [:username, :currencies, :prestige]}
+  @derive {Jason.Encoder,
+           only: [
+             :username,
+             :currencies,
+             :prestige,
+             :most_played_character,
+             :total_kills,
+             :won_matches,
+             :highest_historical_prestige
+           ]}
   schema "users" do
     field(:game_id, :integer)
     field(:username, :string)
@@ -31,7 +40,7 @@ defmodule GameBackend.Users.User do
     field(:last_kaline_afk_reward_claim, :utc_datetime)
     field(:last_dungeon_afk_reward_claim, :utc_datetime)
     field(:profile_picture, :string)
-    field(:prestige, :integer, virtual: true)
+    field(:highest_historical_prestige, :integer)
 
     belongs_to(:dungeon_settlement_level, DungeonSettlementLevel)
     belongs_to(:kaline_tree_level, KalineTreeLevel)
@@ -47,6 +56,12 @@ defmodule GameBackend.Users.User do
     has_many(:currency_caps, UserCurrencyCap)
 
     timestamps()
+
+    # Virtual fields for client rendering
+    field(:prestige, :integer, virtual: true)
+    field(:most_played_character, :string, virtual: true)
+    field(:total_kills, :integer, virtual: true)
+    field(:won_matches, :integer, virtual: true)
   end
 
   @doc false
@@ -64,7 +79,8 @@ defmodule GameBackend.Users.User do
       :level,
       :experience,
       :profile_picture,
-      :google_user_id
+      :google_user_id,
+      :highest_historical_prestige
     ])
     |> cast_assoc(:unlocks)
     |> assoc_constraint(:google_user)
