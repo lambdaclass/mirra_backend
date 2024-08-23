3c3a08862a2999e8f01d37550aefed91f772625b
Nicolas Sanchez <sanchez.nicolas96@gmail.com>
2024-08-23 12:50:57 -0300

    Set gateway_url for existing servers

--- apps/game_backend/priv/repo/migrations/20240823152743_add_gateway_url_field_to_arena_servers.exs
+++ apps/game_backend/priv/repo/migrations/20240823152743_add_gateway_url_field_to_arena_servers.exs
@@ -5,5 +5,10 @@ defmodule GameBackend.Repo.Migrations.AddGatewayUrlFieldToArenaServers do
     alter table(:arena_servers) do
       add :gateway_url, :string
     end
+
+    execute("""
+    UPDATE arena_servers arena_server
+    SET gateway_url = 'https://central-europe-staging.championsofmirra.com';
+    """, "")
   end
 end
