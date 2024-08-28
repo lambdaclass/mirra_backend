ce0c327e0176ac1a7ed6e8c4f9996a5cfcd9c905
Klaus @ LambdaClass <18153834+klaus993@users.noreply.github.com>
2024-08-28 15:50:01 +0000

    Add deployment to Mac Mini in Argentina office (#882)
    * Add MacMini self hosted deploy
    
    * Run with erl daemon rather than launchd
    
    * Export runtime env vars
    
    * Change name
    
    * Remove push trigger
    
    * Swap to testing-argentina environment
    
    * add debug
    
    * Remove debug
    
    * Rename workflow and restore branch name
    
    ---------
    
    Co-authored-by: Nicolas Sanchez <sanchez.nicolas96@gmail.com>

---
+++ devops/deploy-macos.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+. "${HOME}/.cargo/env"
+set -ex
+
+export PATH=${PATH}:/opt/homebrew/bin/
+
+if [ -d "/tmp/mirra_backend" ]; then
+	rm -rf /tmp/mirra_backend
+fi
+
+cd /tmp
+git clone git@github.com:lambdaclass/mirra_backend.git --branch ${BRANCH_NAME} mirra_backend
+cd mirra_backend
+
+chmod +x devops/entrypoint.sh
+
+mix local.hex --force && mix local.rebar --force
+mix deps.get --only $MIX_ENV
+MIX_ENV=$MIX_ENV mix compile
+MIX_ENV=$MIX_ENV mix tailwind configurator --minify
+MIX_ENV=$MIX_ENV mix esbuild configurator --minify
+MIX_ENV=$MIX_ENV mix phx.digest
+mix release ${RELEASE} --overwrite
+if [ ${RELEASE} == "central_backend" ]; then
+	mix ecto.migrate
+fi
+
+export PHX_HOST=$PHX_HOST
+export DATABASE_URL=$DATABASE_URL
+export PHX_SERVER=$PHX_SERVER
+export SECRET_KEY_BASE=$SECRET_KEY_BASE
+export JWT_PRIVATE_KEY_BASE_64=$JWT_PRIVATE_KEY_BASE_64
+export PORT=$PORT
+export RELEASE_NODE=$RELEASE_NODE
+export GATEWAY_URL=$GATEWAY_URL
+export METRICS_ENDPOINT_PORT=$METRICS_ENDPOINT_PORT
+export OVERRIDE_JWT=$OVERRIDE_JWT
+export GOOGLE_CLIENT_ID=$GOOGLE_CLIENT_ID
+export BOT_MANAGER_PORT=$BOT_MANAGER_PORT
+export BOT_MANAGER_HOST=$BOT_MANAGER_HOST
+export CONFIGURATOR_HOST=$CONFIGURATOR_HOST
+export CONFIGURATOR_GOOGLE_CLIENT_ID=$CONFIGURATOR_GOOGLE_CLIENT_ID
+export CONFIGURATOR_GOOGLE_CLIENT_SECRET=$CONFIGURATOR_GOOGLE_CLIENT_SECRET
+export RELEASE=$RELEASE
+export TARGET_SERVER=$TARGET_SERVER
+export LOADTEST_EUROPE_HOST=$LOADTEST_EUROPE_HOST
+export LOADTEST_BRAZIL_HOST=$LOADTEST_BRAZIL_HOST
+
+/Users/lambdaclass/arena/mirra_backend/_build/prod/rel/arena/bin/arena stop
+
+rm -rf ${HOME}/arena/mirra_backend
+mv /tmp/mirra_backend ${HOME}/arena/
+
+/Users/lambdaclass/arena/mirra_backend/_build/prod/rel/arena/bin/arena daemon
