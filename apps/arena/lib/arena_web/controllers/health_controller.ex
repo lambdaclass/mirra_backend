f4dc31098ec89d4fc479f584f96046209568acac
tvillegas98 <tvillegas@fi.uba.ar>
2024-09-12 16:51:10 -0300

    duplicate logic for arena version endpoint

--- apps/arena/lib/arena_web/controllers/health_controller.ex
+++ apps/arena/lib/arena_web/controllers/health_controller.ex
@@ -6,10 +6,12 @@ defmodule ArenaWeb.HealthController do
     |> put_status(:ok)
     |> text("ok")
   end
-
   def version(conn, _params) do
+    arena_version = Application.spec(:arena, :vsn) |> to_string()
+    configurator_version = GameBackend.Configuration.get_configuration_hash_version()
+
     conn
     |> put_status(:ok)
-    |> text(Application.spec(:arena, :vsn))
+    |> text(arena_version <> "." <> configurator_version)
   end
 end
