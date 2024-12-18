de29c4a6cdb97af038a76b098626e7736d56961e
tvillegas98 <tvillegas@fi.uba.ar>
2024-12-18 11:58:06 -0300

    chore: use :math library from erlang

--- apps/bot_manager/lib/math/vector.ex
+++ apps/bot_manager/lib/math/vector.ex
@@ -33,9 +33,9 @@ defmodule BotManager.Math.Vector do
 
   # Using the rotation matrix
   def rotate_by_degrees(%{x: x, y: y}, angle_in_degrees) do
-    angle_in_radians = Math.deg2rad(angle_in_degrees)
-    x = x * Math.cos(angle_in_radians) - y * Math.sin(angle_in_radians)
-    y = x * Math.sin(angle_in_radians) + y * Math.cos(angle_in_radians)
+    angle_in_radians = deg2rad(angle_in_degrees)
+    x = x * :math.cos(angle_in_radians) - y * :math.sin(angle_in_radians)
+    y = x * :math.sin(angle_in_radians) + y * :math.cos(angle_in_radians)
 
     %{
       x: x,
@@ -50,5 +50,10 @@ defmodule BotManager.Math.Vector do
       x: x / distance,
       y: y / distance
     }
+  end
+
+
+  def deg2rad(deg) do
+    deg * :math.pi / 180
   end
 end
