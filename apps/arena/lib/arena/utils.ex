76ff449ac712075d63225db593ed47dd5353bcac
Nicolas Sanchez <sanchez.nicolas96@gmail.com>
2025-01-20 12:16:07 -0300

    Handle float pattern-match for normalize function

--- apps/arena/lib/arena/utils.ex
+++ apps/arena/lib/arena/utils.ex
@@ -80,6 +80,10 @@ defmodule Arena.Utils do
     %{x: 0, y: 0}
   end
 
+  def normalize(%{x: 0.0, y: 0.0}) do
+    %{x: 0, y: 0}
+  end
+
   def normalize(%{x: x, y: y}) do
     length = :math.sqrt(x * x + y * y)
     %{x: x / length, y: y / length}
