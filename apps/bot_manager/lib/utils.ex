65ee9be0cc1b387aa75320d1b80eee5fb64f8293
tvillegas98 <tvillegas@fi.uba.ar>
2025-01-21 18:23:24 -0300

    feat: random position in a circle

--- apps/bot_manager/lib/utils.ex
+++ apps/bot_manager/lib/utils.ex
@@ -6,4 +6,11 @@ defmodule BotManager.Utils do
   def player_alive?(%{aditional_info: {:player, %{health: health}}}), do: health > 0
 
   def player_alive?(_), do: :not_a_player
+
+  def random_position_within_safe_zone_radius(safe_zone_radius) do
+    x = Enum.random(-safe_zone_radius..safe_zone_radius) / 1.0
+    y = Enum.random(-safe_zone_radius..safe_zone_radius) / 1.0
+
+    %{x: x, y: y}
+  end
 end
