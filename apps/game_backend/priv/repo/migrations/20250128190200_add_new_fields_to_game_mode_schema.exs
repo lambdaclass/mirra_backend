e9feb0eca09c9d00f3dc58cab5dd00e8172c209f
tvillegas98 <tvillegas@fi.uba.ar>
2025-02-03 10:26:46 -0300

    chore: remove not used field in migration

--- apps/game_backend/priv/repo/migrations/20250128190200_add_new_fields_to_game_mode_schema.exs
+++ apps/game_backend/priv/repo/migrations/20250128190200_add_new_fields_to_game_mode_schema.exs
@@ -6,7 +6,6 @@ defmodule GameBackend.Repo.Migrations.AddNewFieldsToGameModeSchema do
 
     alter table(:game_mode_configurations) do
       add :name, :string
-      add :respawn_time, :integer
       add :team_enabled, :boolean
       add :team_size, :integer
       add :amount_of_players, :integer
