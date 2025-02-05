5573e5764dcfb4aef4bdce1ab0abe2e47e6ca332
tvillegas98 <tvillegas@fi.uba.ar>
2025-02-05 19:17:13 -0300

    feat: filter maps only in the game mode form

--- apps/game_backend/lib/game_backend/configuration.ex
+++ apps/game_backend/lib/game_backend/configuration.ex
@@ -247,9 +247,7 @@ defmodule GameBackend.Configuration do
 
   """
   def list_map_configurations_by_version(version_id) do
-    Repo.all(
-      from(m in MapConfiguration, where: m.version_id == ^version_id and m.active, order_by: [desc: m.inserted_at])
-    )
+    Repo.all(from(m in MapConfiguration, where: m.version_id == ^version_id, order_by: [desc: m.inserted_at]))
   end
 
   @doc """
