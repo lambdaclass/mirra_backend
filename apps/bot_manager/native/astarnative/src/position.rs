579bf36f9d307d9c3b9414503b70767b54ee32f3
Martin Fraga <martin.fraga@lambdaclass.com>
2025-03-18 17:22:42 -0300

    refactor: remove unused methods

--- apps/bot_manager/native/astarnative/src/position.rs
+++ apps/bot_manager/native/astarnative/src/position.rs
@@ -7,36 +7,11 @@ pub struct Position {
 }
 
 impl Position {
-    pub fn normalize(&mut self) {
-        let length = (self.x.powi(2) + self.y.powi(2)).sqrt();
-        self.x /= length;
-        self.y /= length;
-    }
-
     pub fn add(a: &Position, b: &Position) -> Position {
         Position {
             x: a.x + b.x,
             y: a.y + b.y,
         }
-    }
-    pub fn sub(a: &Position, b: &Position) -> Position {
-        Position {
-            x: a.x - b.x,
-            y: a.y - b.y,
-        }
-    }
-
-    pub fn mult(a: &Position, mult: f32) -> Position {
-        Position {
-            x: a.x * mult,
-            y: a.y * mult,
-        }
-    }
-
-    pub fn distance_to_position(&self, other_position: &Position) -> f32 {
-        let x = self.x - other_position.x;
-        let y = self.y - other_position.y;
-        (x.powi(2) + y.powi(2)).sqrt()
     }
 }
 
