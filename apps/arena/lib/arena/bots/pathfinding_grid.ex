88b66408dfdb26ae7f6abaa50e7cd6d4e1ac4158
Nicolas Sanchez <sanchez.nicolas96@gmail.com>
2025-03-26 12:18:41 -0300

    Add new GenServer to manage bot collision grids

---
+++ apps/arena/lib/arena/bots/pathfinding_grid.ex
@@ -0,0 +1,109 @@
+defmodule Arena.Bots.PathfindingGrid do
+  @moduledoc """
+  Generates a grid for current map.
+  """
+  alias Arena.Configuration
+  use GenServer
+  @update_interval_ms 3_600_000
+  require Logger
+
+  def start_link(_opts) do
+    GenServer.start_link(__MODULE__, [], name: __MODULE__)
+  end
+
+  def init(_opts) do
+    Process.send_after(self(), :update_config, 1_000)
+
+    {:ok, %{}}
+  end
+
+  def get_map_collision_grid(map_name, from_pid) do
+    GenServer.cast(__MODULE__, {:get_map_collision_grid, map_name, from_pid})
+  end
+
+  def handle_info(:update_config, _state) do
+    state =
+      Configuration.get_current_maps_configuration()
+      |> Enum.reduce(%{maps: []}, fn map, acc ->
+        map_obstacles =
+          Enum.reduce(map.obstacles, {[], 1}, fn obstacle, {obstacles_acc, current_id} ->
+            # Add the fake incremental ID to the obstacle
+            updated_obstacle =
+              obstacle
+              |> Map.take([
+                :id,
+                :shape,
+                :position,
+                :radius,
+                :vertices,
+                :speed,
+                :category,
+                :direction,
+                :is_moving,
+                :name
+              ])
+              |> Map.put(:id, current_id)
+              |> Map.put(:shape, get_shape(obstacle.shape))
+              |> Map.put(:category, :obstacle)
+              |> Map.put(:is_moving, false)
+              |> Map.put(:direction, %{x: 0.0, y: 0.0})
+              |> Map.put(:speed, 0.0)
+
+            # Add the updated obstacle to the accumulator list
+            {obstacles_acc ++ [updated_obstacle], current_id + 1}
+          end)
+          # take only the list
+          |> elem(0)
+          |> Enum.map(fn obstacle -> {obstacle.id, obstacle} end)
+          |> Map.new()
+
+        collision_grid =
+          case AStarNative.build_collision_grid(map_obstacles) do
+            {:ok, collision_grid} ->
+              collision_grid
+
+            {:error, reason} ->
+              Logger.error("Grid construction failed with reason: #{inspect(reason)}")
+
+              %{}
+          end
+
+        Map.put(
+          acc,
+          :maps,
+          acc.maps ++
+            [
+              map
+              |> Map.put(:grid, collision_grid)
+            ]
+        )
+      end)
+
+    Process.send_after(__MODULE__, :update_bounties, @update_interval_ms)
+    {:noreply, state}
+  end
+
+  def handle_cast({:get_map_collision_grid, map_name, from_pid}, state) do
+    grid =
+      Enum.find(state.maps, fn map -> map.name == map_name end)
+      |> Map.get(:grid)
+
+    send(from_pid, {:collision_grid_response, grid})
+
+    {:noreply, state}
+  end
+
+  def handle_call({:get_map_collision_grid, map_name}, _from, state) do
+    grid =
+      Enum.find(state.maps, fn map -> map.name == map_name end)
+      |> Map.get(:grid)
+
+    {:reply, grid, state}
+  end
+
+  defp get_shape("polygon"), do: :polygon
+  defp get_shape("circle"), do: :circle
+  defp get_shape("line"), do: :line
+  defp get_shape("point"), do: :point
+  defp get_shape(_), do: nil
+end
