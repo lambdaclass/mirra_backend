8ecfdb4a3b125ee637ce3db989000ab0bf2b5c5f
Martin Fraga <martin.fraga@lambdaclass.com>
2025-03-26 18:29:00 -0300

    feat: add path simplification

--- apps/bot_manager/lib/bot_state_machine.ex
+++ apps/bot_manager/lib/bot_state_machine.ex
@@ -325,6 +325,7 @@ defmodule BotManager.BotStateMachine do
     to = %{x: position_to_move_to.x, y: position_to_move_to.y}
 
     shortest_path = AStarNative.a_star_shortest_path(from, to, bot_state_machine.collision_grid)
+      |> AStarNative.simplify_path(bot_state_machine.obstacles)
       |> SplinePath.smooth_path()
 
     # If we don't have a path, retry finding new position in map
