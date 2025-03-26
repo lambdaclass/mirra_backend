4d6bd4b7e2900568d590013abf4de8708a6b3e06
Nicolas Sanchez <sanchez.nicolas96@gmail.com>
2025-03-26 12:42:05 -0300

    Move variable up for TOC reasons

--- apps/bot_manager/lib/application.ex
+++ apps/bot_manager/lib/application.ex
@@ -7,9 +7,9 @@ defmodule BotManager.Application do
 
   @impl true
   def start(_type, _args) do
+    children = []
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
-    children = []
     opts = [strategy: :one_for_one, name: BotManager.Supervisor]
     Supervisor.start_link(children, opts)
   end
