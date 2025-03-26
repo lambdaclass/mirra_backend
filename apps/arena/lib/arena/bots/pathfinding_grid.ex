463ea50c28c2c349a783e29d3f823313def3e58b
Nicolas Sanchez <sanchez.nicolas96@gmail.com>
2025-03-26 13:05:02 -0300

    Start pathfinding grid genserver only when env-var is enabled

--- apps/arena/lib/arena/bots/pathfinding_grid.ex
+++ apps/arena/lib/arena/bots/pathfinding_grid.ex
@@ -12,7 +12,9 @@ defmodule Arena.Bots.PathfindingGrid do
   end
 
   def init(_opts) do
-    Process.send_after(self(), :update_config, 1_000)
+    if System.get_env("PATHFINDING_TEST") == "true" do
+      Process.send_after(self(), :update_config, 1_000)
+    end
     {:ok, %{}}
   end
 
