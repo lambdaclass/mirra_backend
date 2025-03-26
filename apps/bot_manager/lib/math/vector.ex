2e247642901f90a85690e361cdd3d38b9813148e
Martin Fraga <martin.fraga@lambdaclass.com>
2025-03-26 17:30:18 -0300

    feat: add spline post processing to astar path

--- apps/bot_manager/lib/math/vector.ex
+++ apps/bot_manager/lib/math/vector.ex
@@ -3,6 +3,20 @@ defmodule BotManager.Math.Vector do
   Module to handle math operations with vectors
   """
 
+  def add(vector, value) when is_integer(value) or is_float(value) do
+    %{
+      x: vector.x + value,
+      y: vector.y + value
+    }
+  end
+
+  def add(first_vector, second_vector) do
+    %{
+      x: first_vector.x + second_vector.x,
+      y: first_vector.y + second_vector.y
+    }
+  end
+
   def sub(vector, value) when is_integer(value) or is_float(value) do
     %{
       x: vector.x - value,
