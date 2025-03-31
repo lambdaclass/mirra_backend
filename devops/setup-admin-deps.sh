01cdf22b3441a83c32fcf520b398ab2a6e2e029a
Nicolas Sanchez <sanchez.nicolas96@gmail.com>
2025-03-31 17:39:03 -0300

    Add new Arena server deployment workflow: - All necessary makefile commands to fetch and deploy deps - New scripts to handle bash commands in server

---
+++ devops/setup-admin-deps.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -ex
+
+export SSH_APP_USERNAME=$SSH_APP_USERNAME
+export DATABASE_URL=$DATABASE_URL
+export SECRET_KEY_BASE=$SECRET_KEY_BASE
+export NEWRELIC_KEY=$NEWRELIC_KEY
+
+cd ~/mirra_backend && make admin-setup-arena-server
+
+sudo rm -rf /home/$SSH_APP_USERNAME/mirra_backend
+sudo mv ~/mirra_backend /home/$SSH_APP_USERNAME/
+sudo chown -R $SSH_APP_USERNAME:$SSH_APP_USERNAME /home/$SSH_APP_USERNAME/mirra_backend
+
+sudo mv ~/.ssh/id_ed25519 /home/$SSH_APP_USERNAME/.ssh/
+sudo chown $SSH_APP_USERNAME:$SSH_APP_USERNAME /home/$SSH_APP_USERNAME/.ssh/id_ed25519
