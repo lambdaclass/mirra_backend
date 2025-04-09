dc9fbadf2731f4143edc809f1c555eba749e64c7
Martin Fraga <martin.fraga@lambdaclass.com>
2025-04-09 17:46:18 -0300

    feat: setup base attack in player
    this was an unused field so we're setting this to depend on the level

--- apps/arena/lib/arena/entities.ex
+++ apps/arena/lib/arena/entities.ex
@@ -50,9 +50,10 @@ defmodule Arena.Entities do
       is_moving: false,
       aditional_info: %{
         team: team,
-        health: character.base_health,
-        base_health: character.base_health,
-        max_health: character.base_health,
+        health: (character.base_health + character.base_health * @stat_increase_per_level * (level - 1)) |> round(),
+        base_health: character.base_health + character.base_health * @stat_increase_per_level * (level - 1) |> round(),
+        max_health: character.base_health + character.base_health * @stat_increase_per_level * (level - 1) |> round(),
+        base_attack: character.base_attack + character.base_attack * @stat_increase_per_level * (level - 1) |> round(),
         base_speed: character.base_speed,
         base_radius: character.base_size,
         base_stamina_interval: character.stamina_interval,
