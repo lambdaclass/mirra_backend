bf371413152b950840c1eeb258a76f913b77a669
Manuel Alonso <manuelalonsodesign@gmail.com>
2025-04-09 13:23:40 -0300

    solve conflicts

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1384,7 +1384,6 @@ shinko_basic_params = %{
   kenzu_basic_params,
   otix_corrupt_underground,
   otix_basic_params,
-  shinko_basic_params,
   muflus_basic_params
 ]
 |> Enum.each(fn skin_params -> Characters.insert_skin(skin_params) end)
