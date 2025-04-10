e8b6e226574c97ee9a5b10adf5691b7b9999a043
Manuel Alonso <manuelalonsodesign@gmail.com>
2025-04-10 13:58:48 -0300

    add uren_basic_params to insert skins

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1391,7 +1391,8 @@ uren_basic_params = %{
   kenzu_basic_params,
   otix_corrupt_underground,
   otix_basic_params,
-  muflus_basic_params
+  muflus_basic_params,
+  uren_basic_params
 ]
 |> Enum.each(fn skin_params -> Characters.insert_skin(skin_params) end)
 
