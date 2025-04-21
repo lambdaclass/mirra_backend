6126ad653810818a76b38e8d73647162b1b8a227
Martin Fraga <martin.fraga@lambdaclass.com>
2025-04-21 17:05:41 -0300

    remove commented lines and add mention to source article

--- apps/bot_manager/lib/spline_path.ex
+++ apps/bot_manager/lib/spline_path.ex
@@ -1,6 +1,8 @@
 defmodule SplinePath do
   @moduledoc """
   This module defines methods to generate a spline path out of a waypoint path
+
+  Based on https://qroph.github.io/2018/07/30/smooth-paths-using-catmull-rom-splines.html
   """
 alias BotManager.Math.Vector
 
@@ -30,29 +32,6 @@ alias BotManager.Math.Vector
     |> Enum.map(fn cps -> build_points_for_spline(cps) end)
     |> List.flatten()
   end
-
-  # float t01 = pow(distance(p0, p1), alpha);
-  # float t12 = pow(distance(p1, p2), alpha);
-  # float t23 = pow(distance(p2, p3), alpha);
-
-  # vec2 m1 = (1.0f - tension) *
-  #     (p2 - p1 + t12 * (
-  #         (p1 - p0) / t01 - (p2 - p0) / (t01 + t12)
-  #       )
-  #     );
-  # vec2 m2 = (1.0f - tension) *
-  #     (p2 - p1 + t12 * ((p3 - p2) / t23 - (p3 - p1) / (t12 + t23)));
-  #
-  # Segment segment;
-  # segment.a = 2.0f * (p1 - p2) + m1 + m2;
-  # segment.b = -3.0f * (p1 - p2) - m1 - m1 - m2;
-  # segment.c = m1;
-  # segment.d = p1;
-  #
-  # vec2 point = segment.a * t * t * t +
-  #              segment.b * t * t +
-  #              segment.c * t +
-  #              segment.d;
 
   defp build_points_for_spline([p0, p1, p2, p3]) do
     t01 = :math.pow(Vector.distance(p0, p1), @alpha)
