5d2d32b37ff9c6967680d7fb7fb80eb7da47f019
Nicolas Sanchez <sanchez.nicolas96@gmail.com>
2025-05-06 13:57:14 -0300

    Add functions to gather message queue lengths from desired processes

--- apps/arena/lib/arena/utils.ex
+++ apps/arena/lib/arena/utils.ex
@@ -4,6 +4,8 @@ defmodule Arena.Utils do
   It contains utility functions like math functions.
   """
 
+  alias Arena.Matchmaking
+
   @bot_prefixes [
     "Astro",
     "Blaze",
@@ -140,4 +142,58 @@ defmodule Arena.Utils do
 
   # Time to wait to start game with any amount of clients
   def start_timeout_ms(), do: 4_000
+
+  def message_queue_lengths() do
+    [
+      Matchmaking.DeathmatchMode,
+      Matchmaking.DuoMode,
+      Matchmaking.GameLauncher,
+      Matchmaking.QuickGameMode,
+      Matchmaking.TrioMode,
+      Arena.GameBountiesFetcher,
+      Arena.GameTracker,
+      Arena.Bots.PathfindingGrid
+    ]
+    |> Enum.each(fn registered_name ->
+      Process.whereis(registered_name)
+      |> register_process_info()
+    end)
+
+    register_bots_queues()
+    register_game_updater_queues()
+  end
+
+  defp register_process_info(nil), do: nil
+
+  defp register_process_info(pid) do
+    case Process.info(pid, [:message_queue_len, :registered_name]) do
+      [message_queue_len: len, registered_name: name] -> register_queue_length(name, len)
+      [message_queue_len: len] -> register_queue_length(pid, len)
+      _ -> nil
+    end
+  end
+
+  defp register_queue_length(name, len) do
+    :telemetry.execute([:vm, :message_queue], %{length: len}, %{process: inspect(name)})
+  end
+
+  defp register_bots_queues() do
+    Registry.select(BotRegistry, [{{:"$1", :"$2", :"$3"}, [], [{{:"$1", :"$2"}}]}])
+    |> Enum.each(fn {_bot_id, pid} ->
+      case Process.info(pid, [:message_queue_len]) do
+        [message_queue_len: len] -> :telemetry.execute([:bots, :message_queue], %{length: len}, %{})
+        _ -> nil
+      end
+    end)
+  end
+
+  defp register_game_updater_queues() do
+    Registry.select(GameUpdaterRegistry, [{{:"$1", :"$2", :"$3"}, [], [{{:"$1", :"$2"}}]}])
+    |> Enum.each(fn {_game_id, pid} ->
+      case Process.info(pid, [:message_queue_len]) do
+        [message_queue_len: len] -> :telemetry.execute([:game_updater, :message_queue], %{length: len}, %{})
+        _ -> nil
+      end
+    end)
+  end
 end
