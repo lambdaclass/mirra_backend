35835e8b439005bb6d59ecd2ba3767f4c2bd1283
Martin Fraga <martin.fraga@lambdaclass.com>
2025-05-07 19:01:50 -0300

    replicate telemetry module events using prom ex

--- apps/arena/lib/arena/prom_ex_plugin.ex
+++ apps/arena/lib/arena/prom_ex_plugin.ex
@@ -8,11 +8,40 @@ defmodule Arena.PromExPlugin do
 
   @impl true
   def event_metrics(_opts) do
-    []
+    [
+      Event.build(:game_metrics, [
+        sum("arena.game.count", description: "Number of games in progress"),
+        ## TODO: this metric is an attempt to gather data to properly set the buckets for the distribution metric below
+        last_value("arena.game.tick.duration_measure",
+          description: "Last game tick duration",
+          unit: {:native, :nanosecond}
+        ),
+        ## TODO: Buckets probably need to be redefined, currently they all fall under the first bucket
+        distribution("arena.game.tick.duration",
+          description: "Time spent on running a game tick",
+          unit: {:native, :nanosecond},
+          reporter_options: [buckets: [7_500_000.0, 15_000_000.0, 22_500_000.0]]
+        ),
+        sum("arena.clients.count", description: "Number of clients (websockets) connected")
+      ])
+    ]
   end
 
   @impl true
   def polling_metrics(_opts) do
-    []
+    poll_rate = 10_000
+
+    [
+      Polling.build(:periodic_measurements, poll_rate, {Arena.Utils, :message_queue_lengths, []}, [
+        # All BEAM processes message queues
+        last_value([:vm, :message_queue, :length], tags: [:process]),
+        # Bots
+        distribution([:bots, :message_queue, :length], reporter_options: [buckets: [0, 10, 100, 1000, 10000, 100_000]]),
+        # GameUpdater
+        distribution([:game_updater, :message_queue, :length],
+          reporter_options: [buckets: [0, 10, 100, 1000, 10000, 100_000]]
+        )
+      ])
+    ]
   end
 end
